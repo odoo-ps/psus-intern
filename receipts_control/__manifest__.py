bdb56cb8c3a8dbe9365d663904d4016c99c3b72f
Jigar <jam-odoo@users.noreply.github.com>
2022-05-19 10:50:25 -0700

    Initial commit
    [ADD] Receipts control module
    
    Format code

---
+++ receipts_control/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Receipts Control',
+    'sumary': '',
+    'description': """
+        Control order receipts 
+    """,
+    'author': 'Odoo PS',
+    'category': 'Inventory',
+    'version': '14.0.1.0.0',
+    'depends': ['sale'],
+    'license': 'OPL-1',
+
+}
