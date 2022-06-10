89c3562a57d7f9c7ac20bf36e48bc64297ced0ad
yali-odoo <107009428+yali-odoo@users.noreply.github.com>
2022-06-10 13:04:23 -0400

    Update README.md
    [ADD] onchange function created to check if the user tries to add more products than demanded
    
    [ADD] if statement to verify that only receipts have the validation

---
+++ odoo_inventory/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Odoo Inventory',
+    
+    'sumary': 'Warehouse shouldnt receive more than ordered quantity',
+    
+    'description': """
+        Materiales Castelar: Warehouse shouldn't receive more than ordered quantity
+    """,
+    
+    'author': 'EduardoCedillo(eacm)',
+    
+    'category': 'Inventory',
+    
+    'version': '1.0',
+    
+    'depends': ['stock'],
+    
+    'license' : 'OPL-1',
+    'data': [
+    ],
+    
+   'demo': [
+   ],
+    
+}
