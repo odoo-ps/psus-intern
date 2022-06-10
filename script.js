22995fb5db2164b1f38ddcb3f89e4b5ab0e739c3
Alexa Acosta <alac@odoo.com>
2022-06-10 12:11:20 -0400

    added more listeners/events to script

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
 const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
-const previousOperandTextElement = document.querySelector('[data-previous-operand]');
-const currentOperandTextElement = document.querySelector('[data-current-operand]');
+const prevElem = document.querySelector('[data-previous-operand]');
+const currElem = document.querySelector('[data-current-operand]'); 
 
 class Calculator {
     constructor(prevElem, currElem) {
@@ -101,3 +101,34 @@ class Calculator {
         }
     }
 };
+
+const calculator = new Calculator(prevElem, currElem);
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    })
+});;
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+    })
+});
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute();
+    calculator.updateDisplay();
+});
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear();
+    calculator.updateDisplay();
+});
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete();
+    calculator.updateDisplay();
+});
