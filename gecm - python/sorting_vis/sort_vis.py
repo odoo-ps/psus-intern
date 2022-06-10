14ea0fe8537536e1c23f0116921dbe52f2ce8cb7
gecm-odoo <gecm@odoo.com>
2022-06-10 15:02:42 -0500

    Created sorting algorithm visualizer using pygame to create the UI

---
+++ gecm - python/sorting_vis/sort_vis.py
@@ -0,0 +1,119 @@
+import pygame
+
+win = pygame.display.set_mode((1030, 530))
+
+# initial position
+x = 40
+y = 40
+
+# width of each bar
+width = 40
+
+# height of each bar (data to be sorted)
+height = [200, 50, 130, 90, 250, 61, 110,
+          88, 33, 80, 70, 159, 180, 20,
+          100, 89, 69, 4, 220, 16, 15]
+
+
+def run(method):
+    run = True
+    pygame.init()
+    if method == 'bubble':
+        pygame.display.set_caption("Bubble sort")
+    elif method == 'insertion':
+        pygame.display.set_caption("Insertion sort")
+    else:
+        pygame.display.set_caption("Selection sort")
+
+    # infinite loop
+    while run:
+        execute = False
+        pygame.time.delay(10)
+        keys = pygame.key.get_pressed()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        if keys[pygame.K_SPACE]:
+            execute = True
+        if execute == False:
+            win.fill((0, 0, 0))
+            show(height)
+            pygame.display.update()
+        else:
+            if method == 'bubble':
+                bubble_sort()
+            elif method == 'insertion':
+                insertion_sort(height)
+            else:
+                selection_sort(height, len(height))
+            run = False
+    pygame.quit()
+
+# method to show the list of height
+
+
+def show(height, j=-1):
+
+    # loop to iterate each item of list
+    for i in range(len(height)):
+        if j != -1:
+            if i == j:
+                pygame.draw.rect(win, (255, 0, 0),
+                                 (x + 47 * i, y, width, height[i]*2))
+            else:
+                pygame.draw.rect(win, (255, 255, 255),
+                                 (x + 47 * i, y, width, height[i]*2))
+        else:
+            pygame.draw.rect(win, (255, 255, 255),
+                             (x + 47 * i, y, width, height[i]*2))
+
+
+def bubble_sort():
+    for i in range(len(height) - 1):
+        for j in range(len(height) - i - 1):
+            if height[j] > height[j + 1]:
+                t = height[j]
+                height[j] = height[j + 1]
+                height[j + 1] = t
+                win.fill((0, 0, 0))
+                show(height, j+1)
+                pygame.time.delay(50)
+                pygame.display.update()
+
+
+def insertion_sort(array):
+    for i in range(1, len(array)):
+        key_item = array[i]
+        j = i - 1
+        while j >= 0 and array[j] > key_item:
+            array[j + 1] = array[j]
+            win.fill((0, 0, 0))
+            show(array, j+1)
+            pygame.time.delay(50)
+            pygame.display.update()
+            j -= 1
+        array[j + 1] = key_item
+        win.fill((0, 0, 0))
+        show(array, j+1)
+        pygame.time.delay(50)
+        pygame.display.update()
+
+
+def selection_sort(array, size):
+    for step in range(size):
+        min_idx = step
+        for i in range(step + 1, size):
+            # to sort in descending order, change > to < in this line
+            if array[i] < array[min_idx]:
+                min_idx = i
+            win.fill((0, 0, 0))
+            show(array, min_idx)
+            pygame.time.delay(50)
+            pygame.display.update()
+
+        # put min at the correct position
+        (array[step], array[min_idx]) = (array[min_idx], array[step])
+        win.fill((0, 0, 0))
+        show(array, min_idx)
+        pygame.time.delay(50)
+        pygame.display.update()
---
+++ gecm - python/sorting_vis/start.py
@@ -0,0 +1,68 @@
+import pygame
+import sort_vis
+
+
+pygame.init()
+width, height = (1030, 530)
+screen = pygame.display.set_mode((width, height))
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (30, 30, 30)
+FONT = pygame.font.Font("freesansbold.ttf", 50)
+
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, BLACK)
+    return textSurface, textSurface.get_rect()
+
+
+def loop():
+    clock = pygame.time.Clock()
+    bubble = pygame.Rect(10, 10, 500, 250)
+    selection = pygame.Rect(520, 10, 500, 250)
+    insertion = pygame.Rect(10, 270, 500, 250)
+    done = False
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    if bubble.collidepoint(event.pos):
+                        # Run bubble sort
+                        sort_vis.run('bubble')
+                    if selection.collidepoint(event.pos):
+                        # Run selection sort
+                        sort_vis.run('selection')
+                    if insertion.collidepoint(event.pos):
+                        # Run insertion sort
+                        sort_vis.run('insertion')
+
+        screen.fill(BLACK)
+        pygame.draw.rect(screen, WHITE, bubble)
+        pygame.draw.rect(screen, WHITE, selection)
+        pygame.draw.rect(screen, WHITE, insertion)
+
+        smallText = pygame.font.Font("freesansbold.ttf", 20)
+        textSurf, textRect = text_objects("BUBBLE", smallText)
+        textRect.center = (bubble.center)
+        screen.blit(textSurf, textRect)
+
+        smallText = pygame.font.Font("freesansbold.ttf", 20)
+        textSurf, textRect = text_objects("SELECTION", smallText)
+        textRect.center = (selection.center)
+        screen.blit(textSurf, textRect)
+
+        smallText = pygame.font.Font("freesansbold.ttf", 20)
+        textSurf, textRect = text_objects("INSERTION", smallText)
+        textRect.center = (insertion.center)
+        screen.blit(textSurf, textRect)
+
+        pygame.display.update()
+
+        clock.tick(30)
+
+
+loop()
+pygame.quit()
