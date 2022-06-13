ae102724dc6db1b8f97a65ba19d0562a98f8c825
jden <jden@odoo.com>
2022-06-13 16:55:48 +0000

    [FIX]python syntax error

--- training-library-module/models/.ipynb_checkpoints/books-checkpoint.py
+++ training-library-module/models/.ipynb_checkpoints/books-checkpoint.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields
 
 class Books(models.Model):
     _name = 'library.books'
@@ -10,7 +10,7 @@ class Books(models.Model):
     authors = fields.Char(string='Authors',required=True)
     editors = fields.Char(string='Editors',required=True)
     publisher = fields.Char(string="Publisher", required=True)
-    ISBN = fields.Float(string="ISBN"), required=True
+    ISBN = fields.Float(string="ISBN", required=True)
     genre = fields.Selection([
         ('action', 'Action'),
         ('classic', 'Classic'),
