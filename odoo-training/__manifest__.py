833c8fbb80bed3c344b7521dcfa6188f7dd9e614
jden <jden@odoo.com>
2022-06-13 14:07:59 +0000

    [ADD]training module

---
+++ odoo-training/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'odoo training module',
+    'summary': 'module created for onboarding training purposes',
+    'author': 'Justin Deng (jden)',
+    'category': 'Training',
+    'depends': ['base'],
+    'data':[],
+    'demo':[]
+}
