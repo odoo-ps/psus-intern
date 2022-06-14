ad432ec7c24cdcbd688354c20eb87066e14b7e06
daav-odoo <107005161+daav-odoo@users.noreply.github.com>
2022-06-13 21:42:50 -0500

    Delete JavaScript directory

--- JavaScript/index.js
+++
@@ -1,4 +0,0 @@
- //Importamos una funcion en este archivo
- import { capitalizeString } from "./string_function.js"
- const cap = capitalizeString("Buenas!");
- console.log(cap);
--- JavaScript/string_function.js
+++
@@ -1,2 +0,0 @@
-//Export, exportamos la funcion a otro archivo
-export const capitalizeString = str => str.toUpperCase()
