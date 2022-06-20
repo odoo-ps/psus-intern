1a177841581f2da1db2f6f4f2c8b8a936cfa3e66
ayhp-odoo <ayhp@odoo.com>
2022-06-20 11:50:16 -0500

    removed foreign files

---
+++ TVU-Networks-Auto-Cancel-Expired-Quotations/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- Coding: utf-8 -*-
+{
+  'name': 'TVU Networks',
+  'summary': '''Administrative app to auto cancel expired quotations''',
+  'description': '''
+    TBD
+  ''',
+  'author': 'Odoo',
+  'website': 'https://www.odoo.com',
+  'category': 'Administrative',
+  'version': '0.1',
+  'depends': ['base'],
+  'data': [],
+  'demo': [],
+}
