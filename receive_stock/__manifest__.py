af32116422d4364f020b99f18f0df0b63f33e124
gecm-odoo <gecm@odoo.com>
2022-06-20 16:34:53 -0500

    [ADD] Added a module to filter received qty vs ordered qty
    Added the receive_stock module to filter inventory stock. The filter limits the received quantity of a product in a sale order to be less than the ordered quantity.
    If Received quantity (quantity_done) is greater than ordered quantity (product_uom_qty) then a ValidationError pops up.
    This module inherits from stock.move.

---
+++ receive_stock/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Receive Stock',
+    'summary': """Stock app to manage stock""",
+    'description': """
+        Module to avoid errors in receiving stock in inventory:
+        1. Being able to receive more stock than the order quantity
+    """,
+    'author': 'Odoo',
+    'website': 'https://www.odoo.com',
+    'category': 'Training',
+    'version': '15.0.1.0.0',
+    'depends': ['delivery'],
+    'data': [
+
+    ],
+    'demo': [
+
+    ],
+    'license': 'OPL-1',
+}
---
+++ receive_stock/models/stock_picking.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.move'
+
+    @api.onchange('product_uom_qty', 'quantity_done')
+    def _onchange_quantity_done(self):
+        if self.quantity_done > self.product_uom_qty:
+            raise ValidationError(
+                _(f'You can\'t receive more than the ordered quantity. Please, enter another quantity.\nOrdered quantity: {self.product_uom_qty}\nReveived quantity: {self.quantity_done}'))
