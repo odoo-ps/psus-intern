dd1fa7e09d5c499126c7263609012701e2551c6c
damu-odoo <damu@odoo.com>
2022-06-20 15:25:36 -0500

    [ADD] Receipts control module

---
+++ receipts_control/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Receipts Control',
+    'sumary': '',
+    'description': """
+        Control order receipts 
+    """,
+    'author': 'damu',
+    'category': 'Inventory',
+    'version': '0.1',
+    'depends': ['sale'],
+    'license' : 'OPL-1',
+    'data': [
+    
+    ],
+   'demo': [
+   
+   ],
+    
+}
