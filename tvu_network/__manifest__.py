13cb0010bd38387fb3f3177bb31333d882ddbf70
EduardoCedillo(eacm) <eacm@odoo.com>
2022-06-20 11:58:07 -0500

    [ADD]Cron task created to cancel all quotations that are expired unable to test it, moving to the next task
    [DEL] delete files that are no longer used
    
    [ADD] format the files with a nice style
    
    [ADD] change the _remove_quotation_if_expired_autovacuum to use the action_cancel function from sale.order
    
    [FIX] remove unused attributes in __manifest__.py

--- tvu_network/__manifest__.py
+++ tvu_network/__manifest__.py
@@ -1,26 +1,21 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Auto cancel',
-    
+
     'sumary': 'Auto cancel expired quotations',
-    
+
     'description': """
         Auto cancel app to cancel expired quotations:
     """,
-    
+
     'author': 'EduardoCedillo(eacm)',
-    
+
     'category': 'Sales',
-    
-    'version': '1.1',
-    
+
+    'version': '15.0.1.0.0',
+
     'depends': ['sale'],
-    
-    'license' : 'OPL-1',
-    'data': [
-    ],
-    
-   'demo': [
-   ],
-    
+
+    'license': 'OPL-1',
+
 }
