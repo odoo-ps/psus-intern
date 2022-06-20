cfe0419abfa19298fc85d2d8c504347a19d489e8
hmpb-odoo <hmpb@odoo.com>
2022-06-20 15:09:01 -0500

    [FIX]  fixing errors

--- inventary_check/__manifest__.py
+++ inventary_check/__manifest__.py
@@ -1,28 +1,21 @@
 # -*- coding: utf-8 -*-
 {
     'name':'Stock check',
-    
     'sumary':'App to manage a stock',
-    
     'description':"""
         Stock check app to verify that the stock is correct:
         -Inventary
         -Purchase order
     """,
-    
     'author':'Odoo PS',
-    
     'category':'Stock',
-    
-    'version':'14.0',
-    
-    'depends':['stock'],
-    
+    'version':'14.0.1.0.0',
+    'depends':[
+        'stock',
+        ],
     'license' :'OPL-1',
     'data':[
     ],
-    
    'demo':[
    ],
-    
 }
