7b0bc14d5ee236d5ba8322b3f85b2e2d3c0133a1
ayhp-odoo <ayhp@odoo.com>
2022-06-20 18:40:09 -0500

    avoided receive more than ordered quantity

---
+++ materials-castelar/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- Encoding: utf-8 -*-
+{
+  'name': 'Materials Castelar',
+  'summary': '''Odoo psus''',
+  'description': '''ayhp''',
+  'author': 'Odoo',
+  'website': 'https://www.odoo.com',
+  'category': 'Inventory',
+  'version': '0.1.0',
+  'depends': ['stock'],
+  'data': [],
+  'demo': [],
+  'license': 'OPL-1'
+}
