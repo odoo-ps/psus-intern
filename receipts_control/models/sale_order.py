38b7d1d1bb192ccbc51f955391a02aa12d6458df
damu-odoo <damu@odoo.com>
2022-06-20 16:24:57 -0500

    Fixed conflicts in sale_order, sale_report

--- receipts_control/models/sale_order.py
+++ receipts_control/models/sale_order.py
@@ -3,4 +3,10 @@
 from odoo import models, fields, api
 
 class SaleOrder(models.Model):
+<<<<<<< HEAD
     _inherit = 'sale.order'
+=======
+    _inherit = 'sale.order'
+    
+    
+>>>>>>> dd1fa7e ([ADD] Receipts control module)
--- receipts_control/models/sale_report.py
+++ receipts_control/models/sale_report.py
@@ -3,4 +3,10 @@
 from odoo import models, fields, api
 
 class SaleReport(models.Model):
+<<<<<<< HEAD
     _inherit = 'sale.report'
+=======
+    _inherit = 'sale.report'
+    
+    
+>>>>>>> dd1fa7e ([ADD] Receipts control module)
