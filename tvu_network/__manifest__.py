e3427d335f8101fd04a88c50b4d0bd5291e3e615
hmpb-odoo <hmpb@odoo.com>
2022-06-20 16:14:28 -0500

    [FIX] Fixing wrong functionality

--- tvu_network/__manifest__.py
+++ tvu_network/__manifest__.py
@@ -1,25 +1,19 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Auto cancel',
-    
     'sumary': 'Auto cancel expired quotations',
-    
     'description': """
         Auto cancel app to cancel expired quotations:
     """,
-    
     'author': 'hmpb',
-    
     'category': 'Sales',
-    
     'version': '0.1',
-    
-    'depends': ['sale'],
-    
+    'depends': [
+        'sale',
+        ],
     'license' : 'OPL-1',
     'data': [
     ],
-    
    'demo': [
    ],
     
--- tvu_network/models/cancel.py
+++ tvu_network/models/cancel.py
@@ -7,11 +7,5 @@ class Cancel(models.Model):
 
     #check if the quotation is expired
     @api.autovacuum
-    def check_expired(self):
-        for record in self:
-            if record.type_name == 'Quotation':
-                if record.validity_date:
-                    if(record.validity_date < fields.Date.today() or record.is_expired):
-                        record.state = 'cancel'
-                        record.is_expired = True
-    
+    def _check_expired(self):
+        self.env['sale.order'].search([('state', '=', 'draft')]).filtered(lambda r: r.validity_date and r.validity_date < fields.Date.today()).action_cancel()
