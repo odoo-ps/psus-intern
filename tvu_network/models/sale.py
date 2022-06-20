1fa1b6e595fa5ce34bb7a0d33d377496ca60b9c1
EduardoCedillo(eacm) <eacm@odoo.com>
2022-06-20 11:58:07 -0500

    [ADD]Cron task created to cancel all quotations that are expired unable to test it, moving to the next task
    [DEL] delete files that are no longer used
    
    [ADD] format the files with a nice style
    
    [ADD] change the _remove_quotation_if_expired_autovacuum to use the action_cancel function from sale.order
    
    [FIX] remove unused attributes in __manifest__.py

--- tvu_network/models/sale.py
+++ tvu_network/models/sale.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
 
+
 class Sale(models.Model):
     _inherit = 'sale.order'
 
-    #cancelled the quotations that are expired every day
+    # cancelled the quotations that are expired every day
     @api.autovacuum
     def _remove_quotation_if_expired_autovacuum(self):
-        self.env['sale.order'].search([('state', '=', 'draft'), ('validity_date', '<', fields.Date.today())]).write({'state': 'cancel'})
+        # use the action_cancel() function
+        self.env['sale.order'].search(
+            [('state', '=', 'draft'), ('validity_date', '<', fields.Date.today())]).action_cancel()
