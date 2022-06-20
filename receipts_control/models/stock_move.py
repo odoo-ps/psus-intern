b3a64f8de12fdaa9f9901b046d91be2b22d76198
damu-odoo <damu@odoo.com>
2022-06-20 12:39:29 -0500

    [ADD] Receipts Control Module

---
+++ receipts_control/models/stock_move.py
@@ -0,0 +1,13 @@
+# *-* coding: utf-8 *-*
+
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError, UserError
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+    
+    
+    @api.onchange('quantity_done')
+    def check_quantity_done(self):
+        if self.quantity_done > self.product_uom_qty:
+            raise ValidationError("You can't receive more than the ordered quantity. Please, enter another quantity")
