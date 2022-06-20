2a26cd9124a8f805782184b0811ef5fc1eb58685
EduardoCedillo(eacm) <eacm@odoo.com>
2022-06-20 16:34:18 -0500

    [ADD] format the files with a nice style

--- tvu_network/models/sale.py
+++ tvu_network/models/sale.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
 
+
 class Sale(models.Model):
     _inherit = 'sale.order'
 
-    #cancelled the quotations that are expired every day
+    # cancelled the quotations that are expired every day
     @api.autovacuum
     def _remove_quotation_if_expired_autovacuum(self):
-        self.env['sale.order'].search([('state', '=', 'draft'), ('validity_date', '<', fields.Date.today())]).write({'state': 'cancel'})
+        self.env['sale.order'].search(
+            [('state', '=', 'draft'), ('validity_date', '<', fields.Date.today())]).write({'state': 'cancel'})
