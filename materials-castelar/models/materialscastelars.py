55bf3e195d2484611231f32624f889317a3956bc
romf <romf@odoo.com>
2022-06-21 09:51:47 -0500

    corrections regarding the comments done

--- materials-castelar/models/materialscastelars.py
+++ materials-castelar/models/materialscastelars.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
 
 class Order(models.Model):
@@ -6,6 +6,7 @@ class Order(models.Model):
 
     @api.onchange('product_uom_qty','quantity_done')
     def _onchange_quantity_done(self):
+        self.ensure_one()
         if self.picking_code == 'incoming':
             if self.quantity_done > self.product_uom_qty:
-                raise ValidationError("Error you cant output more than the order quantity")
+                raise ValidationError(_("Error you cant output more than the order quantity"))
