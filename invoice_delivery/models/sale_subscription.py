b0aa473b8120e29757759b01e7350bfc963157a0
gecm-odoo <gecm@odoo.com>
2022-06-21 15:14:49 -0500

    [FIX] Edge case for renewal order values
    There was a method that also needed to be updated with the delivery address, it is the same proccess as the previous one but with the _prepare_renewal_order_values method

--- invoice_delivery/models/sale_subscription.py
+++ invoice_delivery/models/sale_subscription.py
@@ -12,6 +12,12 @@ class SaleSubscription(models.Model):
     def _prepare_invoice_data(self):
         self.ensure_one()
         vals = super()._prepare_invoice_data()
-        vals['partner_shipping_id'] = self.property_partner or vals.get(
+        vals['partner_shipping_id'] = self.property_partner.id or vals.get(
             'partner_shipping_id')
         return vals
+
+    def _prepare_renewal_order_values(self, discard_product_ids=False, new_lines_ids=False):
+        res = super()._prepare_renewal_order_values(discard_product_ids, new_lines_ids)
+        for subscription in self.filtered('property_partner'):
+            res[subscription.id]['partner_shipping_id'] = self.property_partner.id
+        return res
