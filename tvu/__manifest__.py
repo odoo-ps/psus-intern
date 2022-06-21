ad0be0774377ff2cc58c83bb82de8974c912e46d
daav-odoo <107005161+daav-odoo@users.noreply.github.com>
2022-06-21 15:44:06 -0500

    tvu

---
+++ tvu/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+{
+
+    'name': 'Auto-remove quotations',
+
+    'summary': """Auto-Cancel Expired Quotations""",
+
+    'description': """
+
+       
+    """,
+
+    'author': 'daav-odoo',
+
+    'website': 'https://www.odoo.com',
+
+    'category': 'Training',
+
+    'version': '0.1',
+
+    'depends': ['sale'],
+
+    'data': [
+       
+],
+
+    'demo': [
+
+],
+
+}
