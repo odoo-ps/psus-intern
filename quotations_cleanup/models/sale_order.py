5b4253bc930b122b3649bc1ea4d022c8c7527084
Viet Dinh <vidi@odoo.com>
2022-06-21 10:23:13 -0400

    [FIX] Lint

--- quotations_cleanup/models/sale_order.py
+++ quotations_cleanup/models/sale_order.py
@@ -1,7 +1,9 @@
 from odoo import models
 
+
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     def _unlink_expired_quotations(self):
-        self.search([('is_expired', '=', True), ('state', '=', 'draft')]).unlink()
+        self.search([('is_expired', '=', True),
+                    ('state', '=', 'draft')]).unlink()
