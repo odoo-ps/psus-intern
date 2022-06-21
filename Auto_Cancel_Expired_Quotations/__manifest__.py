a0d17b0e25709ad24c0b02066ce1eb84ca37f637
romf <romf@odoo.com>
2022-06-21 10:30:16 -0500

    corrections rergarding comments

--- Auto_Cancel_Expired_Quotations/__manifest__.py
+++ Auto_Cancel_Expired_Quotations/__manifest__.py
@@ -2,29 +2,14 @@
 {
     'license': 'LGPL-3',
     'name': 'Auto-Cancel Expired Quotations',
-    
     'summary': """Module to Auto cancel Expired Quotations""",
-    
     'description': """Module to Auto cancel Expired Quotations""",
-    
     'author': 'romf',
-    
     'website':'https://www.odoo.com',
-    
     'category': 'Administrative',
-    
-    'version': '0.1',
-    
-    'depends': ['sale'],
-    
-    'license' : 'OPL-1',
-    
-    'data': [
-        'views/auto_cancel_menuitems.xml'
+    'version': '15.0.1.0.0',
+    'depends': [
+        'sale',
     ],
-    
-    'demo': [
-        
-    ],
-    
+    'license' : 'OPL-1', 
 }
