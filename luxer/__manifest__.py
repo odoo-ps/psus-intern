675a65a222d1edf96bc8de5dda7d150674544c71
daav-odoo <107005161+daav-odoo@users.noreply.github.com>
2022-06-21 15:39:36 -0500

    luxer

---
+++ luxer/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+
+    'name': 'Luxer',
+
+    'author': 'daav-odoo',
+
+    'website': 'https://www.odoo.com',
+
+    'category': 'Training',
+
+    'version': '0.1',
+
+    'depends': [
+        "sale_subscription",
+    ],
+
+    'data': [  
+         
+],
+
+    'demo': [
+],
+
+}
