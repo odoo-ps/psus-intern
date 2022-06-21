cf12d150c76e15878f63996fcddda52ec83efa1f
teth-odoo <teth@odoo.com>
2022-06-21 15:38:54 -0400

    Auto Cancel The Expired Quotations

--- tvu-networks/models/sale_order.py
+++ tvu-networks/models/sale_order.py
@@ -2,6 +2,7 @@
 from odoo import models,fields,api
 # from datetime import datetime
 # from logger import logging 
+
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
     def _auto_cancel_expired_record(self):
