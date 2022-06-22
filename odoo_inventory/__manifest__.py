6d22a0722ed6122915066561feff3c89004691f6
EduardoCedillo(eacm) <eacm@odoo.com>
2022-06-22 11:52:31 -0500

    [FIX] erase unused lists in __manifest__

--- odoo_inventory/__manifest__.py
+++ odoo_inventory/__manifest__.py
@@ -1,26 +1,14 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Odoo Inventory',
-
     'sumary': 'Warehouse shouldnt receive more than ordered quantity',
-
     'description': """
         Materiales Castelar: Warehouse shouldn't receive more than ordered quantity
     """,
-
     'author': 'EduardoCedillo(eacm)',
-
     'category': 'Inventory',
-
     'version': '14.0.1.0.0',
-
     'depends': ['stock'],
-
     'license': 'OPL-1',
-    'data': [
-    ],
-
-    'demo': [
-    ],
 
 }
