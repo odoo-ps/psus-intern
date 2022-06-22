6140f9936ceff8e32dcaced1e0cd3d9c38e610a4
Orrin Zhong <orzh@odoo.com>
2022-06-22 16:44:22 -0400

    Fixed method names

--- cancel_quote_module/models/sale_order.py
+++ cancel_quote_module/models/sale_order.py
@@ -13,7 +13,7 @@ class SaleOrder(models.Model):
         current_date = fields.Date.today()
         return current_date > expire_date
 
-    def auto_cancel(self):
+    def auto_quotation_cancel(self):
         all_quotes = self.search([])
         quotes_to_cancel = all_quotes.filtered(self.check_expired)
         quotes_to_cancel._action_cancel()
