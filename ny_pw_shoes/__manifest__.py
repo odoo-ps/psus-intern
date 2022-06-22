368fae9af4e45ff054c2cbe621041b4bd0f85f11
wiwu-odoo <wiwu@odoo.com>
2022-06-22 13:08:41 -0400

    Initial Commit

---
+++ ny_pw_shoes/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'NY P&W Shoes',
+
+    'application': True,
+
+    'summary': """App to calculate price of shoes""",
+
+    'desciption' : """description""",
+
+    'author': 'wiwu',
+
+    'category': 'Inventory',
+
+    'version': '0.1',
+
+    'depends': ['sale'],
+
+    'license': 'GPL-3',
+
+    'data': [
+        'views/product_views.xml',
+    ],
+   'demo': [
+   
+   ],
+}
