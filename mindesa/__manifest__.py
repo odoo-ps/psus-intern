1a76decfd1d7e21dfea6df71ab31063f562b67bc
ayhp-odoo <ayhp@odoo.com>
2022-06-22 15:48:51 -0500

    added functionality for auto confirm RFQs

---
+++ mindesa/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- Encoding: utf-8 -*-
+{
+  'name': 'Mindesa App',
+  'summary': '''Scheduled Action to Confirm RFQs''',
+  'description': '''
+    Module to Scheduled Action that will confirm all RFQs into PO
+  ''',
+  'author': 'Odoo',
+  'website': 'https://www.odoo.com',
+  'category': 'Training',
+  'version': '14.0.1.0.0',
+  'depends': [
+    'sale',
+    'sale_subscription',
+    'product',
+    'purchase',
+  ],
+  'data': ['views/confirm_rdqs_scheduler.xml'],
+  'license': 'OPL-1'
+}
---
+++ mindesa/models/purchase_order.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models
+
+class PurchaseOrder(models.Model):
+  _inherit = "purchase.order"
+
+  test = fields.Char()
+
+  def confirm_rfqs(self):
+    orders = self.env['purchase.order'].search([
+      ('state', '=', 'draft'), 
+      ('user_id.id', '=', 2), 
+      ('partner_id.id', '=', 1)
+    ])
+    for order in orders:
+      order.button_confirm()
