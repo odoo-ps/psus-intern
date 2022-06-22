f7a634a4daf2b614d048ec68e1862876f1feb087
Philip Snyder <phsn@odoo.com>
2022-06-22 12:16:49 -0400

    [IMP] Changed field names

--- models/sale_subscription_partner.py
+++ models/sale_subscription_partner.py
@@ -5,14 +5,14 @@ from odoo import fields, models, api
 class SubscriptionSalePartner(models.Model):
     _inherit = 'sale.subscription'
 
-    property_partner = fields.Many2one(comodel_name='res.partner',string="Property Partner")
+    prop_partner = fields.Many2one(comodel_name='res.partner',string="Property Partner")
 
     def _recurring_create_invoice(self, *args, **kwargs):
         invoices = super()._recurring_create_invoice(*args, **kwargs)
         
         for invoice in invoices:
-            if self.property_partner:
-                invoice.update({'prop_partner': self.property_partner})
+            if self.prop_partner:
+                invoice.update({'prop_partner': self.prop_partner})
 
         return invoices
 
