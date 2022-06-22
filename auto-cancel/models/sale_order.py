576ecd3ef7d9daac3fd9f3b5f2932f8ff54eb794
Alexa Acosta <107050329+alac-odoo@users.noreply.github.com>
2022-06-22 12:40:18 -0400

    [REM] sales: removed some irrelevant comments

--- auto-cancel/models/sale_order.py
+++ auto-cancel/models/sale_order.py
@@ -7,11 +7,6 @@ from odoo import api, fields, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    # Retrieve Sale Order records (self.env['sale.order'])
-    # Find Quotations among records (state=draft)
-    # Filter expired Quotations (is_expired=True)
-    # Cancel Quotations that meet the conditions (action_cancel())
-    # Should leave Quotations with no expiration date set alone
     @api.autovacuum
     def _auto_cancel_expired_quotations(self):
         self.env['sale.order'].search(
