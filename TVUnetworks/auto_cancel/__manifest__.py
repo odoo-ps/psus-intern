e92962e96106282fcc26d475fe1b3ac153853589
yixi-odoo <yixi@odoo.com>
2022-06-22 10:28:01 -0400

    [ADD] auto cancelation

---
+++ TVUnetworks/auto_cancel/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'TVU Networks',
+    'summary': """cancel expired quotations  """,
+    'description': """
+        canceling expired quotations
+    """,
+    'Author' : 'yixi',
+    'website' : 'https://www.odoo.com',
+    'category' : 'Training',
+    'version' : '0.1',
+    'depends': ['base','sales'],
+    'data' : [
+        'views/.xml'
+    ],
+    'license': "OPL-1",
+    'demo': [],
+
+}
