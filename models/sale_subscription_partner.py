624d7b7372763db55ddd306ef9f8bb3163bd35ec
Philip Snyder <phsn@odoo.com>
2022-06-22 11:08:37 -0400

    [IMP] Update invoice partner_id

--- models/sale_subscription_partner.py
+++ models/sale_subscription_partner.py
@@ -8,7 +8,15 @@ class SubscriptionSalePartner(models.Model):
     property_partner = fields.Many2one(comodel_name='res.partner',string="Property Partner")
 
     def _recurring_create_invoice(self, *args, **kwargs):
-        print("Logged call")
-        return super()._recurring_create_invoice(*args, **kwargs)
+        invoices = super()._recurring_create_invoice(*args, **kwargs)
+        
+        for invoice in invoices:
+            if self.property_partner:
+                # invoice delivery address managed by partner_id
+                # or add aditional delivery_address field to invoice and hook _get_invoice_delivery_partner_id
+                # ticket not specific enough
+                invoice.update({'partner_id': self.property_partner})
+
+        return invoices
 
     
