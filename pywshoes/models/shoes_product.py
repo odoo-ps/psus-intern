e306bfe0df5ecfc6abbb73c70001294c3a7309b5
romf <romf@odoo.com>
2022-06-22 13:42:22 -0500

    fix comments issues

--- pywshoes/models/shoes_product.py
+++ pywshoes/models/shoes_product.py
@@ -9,14 +9,9 @@ class ProductTemplate(models.Model):
     
     pair_per_case = fields.Integer(string="pair_per_case")
     price_per_pair = fields.Monetary(string="price_per_pair")
-    
-    state = fields.Selection([('editable', 'Editable'), ('read_only',
-                              'Read Only')], 'Status', copy=False, default='editable', readonly=True)
-
     list_price = fields.Float(compute='_compute_list_price',
                               inverse='_inverse_list_price',
-                              store=True,
-                              states={'editable': [('readonly', False)], 'read_only': [('readonly', True)]})
+                              store=True,)
     
     
     
@@ -25,6 +20,3 @@ class ProductTemplate(models.Model):
         self.ensure_one()
         if self.pair_per_case != 0 and self.price_per_pair != 0:
             self.list_price = self.pair_per_case*self.price_per_pair
-            self.state='read_only'
-        else:
-            self.state='editable'
