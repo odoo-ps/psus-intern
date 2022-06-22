55c30b962416107721ea6b1561b8302c6e908ecb
hmpb-odoo <hmpb@odoo.com>
2022-06-22 11:03:13 -0500

    [ADD] Product template model
    [FIX] Fixing a bug
    
    [FIX] Fix a bug with the auto calculated price

--- pw_shoes/models/product_template.py
+++ pw_shoes/models/product_template.py
@@ -1,7 +1,7 @@
 # -- coding: utf-8 --
 
 from odoo import api, fields, models, _
-from odoo.exceptions import ValidationError
+from odoo.exceptions import UserError
 
 
 class ProductTemplate(models.Model):
@@ -10,21 +10,11 @@ class ProductTemplate(models.Model):
     pair_per_case = fields.Integer(string='Pairs per case')
     price_per_pair = fields.Float(string='Price per pair')
 
-    list_price = fields.Float(string='Sales price', compute='_compute_list_price',
-                              readonly=True,
-                              store=True,
-                              states={'editable': [('readonly', False)], 'read_only': [('readonly', True)]})
-    state = fields.Selection([('editable', 'Editable'), ('read_only',
-                                                         'Read Only')], 'Status', copy=False, default='editable')
+    list_price = fields.Float(string='Sales price', compute='_compute_list_price', store=True)
 
     @api.depends('price_per_pair', 'pair_per_case')
     def _compute_list_price(self):
-        self.ensure_one()
-        if(self.price_per_pair < 0 or self.pair_per_case < 0):
-            raise ValidationError(
-                ('Price per pair and pair per case must be positive'))
-        elif(self.price_per_pair == 0 and self.pair_per_case == 0):
-            self.state = 'editable'
-        else:
-            self.state = 'read_only'
-            self.list_price = self.price_per_pair *  self.pair_per_case
+        for product in self.filtered(lambda x: x.price_per_pair and x.pair_per_case):
+            if(product.pair_per_case < 0 or product.price_per_pair < 0):
+                raise UserError(_('Pair per case and price per pair must be positive'))
+            product.list_price = product.price_per_pair * product.pair_per_case
