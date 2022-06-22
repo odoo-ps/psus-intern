1a0b3e5eb32a1fdd52210d4c7dcd398ed9bdbf7a
sast-odoo <sast@odoo.com>
2022-06-22 14:26:46 -0400

    relocate to correct repo

---
+++ tvu/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name" : "TVU",
+    
+    "depends" : ["sale"],
+
+    "data" : [
+        "views/sale_views_inherit.xml",
+        "data/sale_order_data.xml"
+    ],
+
+    "demo" : []
+
+}
