c7ca420fa6a3e727c8df11c93508ebe89ee2189e
damu-odoo <damu@odoo.com>
2022-06-22 11:34:26 -0500

    author damu-odoo <damu@odoo.com> 1655915666 -0500 committer damu-odoo <damu@odoo.com> 1656008153 -0500
    Initial Commit
    
    Modified manifest, added translation to the validation error
    
    Rebase
    
    Initial commit
    
    Modified manifest, added translation to the validation error
    
    Removed pycache

---
+++ receipts_control/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Receipts Control',
+    'description': """
+        Control order receipts. Warehouse shouldn't receive more than the ordered products
+    """,
+    'author': 'Odoo PS',
+    'category': 'Stock',
+    'version': '14.0.1.0.0',
+    'depends': ['sale'],
+    'license': 'OPL-1',
+
+}
