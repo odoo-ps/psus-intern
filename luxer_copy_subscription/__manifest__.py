01d1a899871b5afa545d15cfcfb1b09c2b2796af
yall-odoo <yall@odoo.com>
2022-06-22 13:51:14 -0500

    [CHG] sale_subscription inheritance
    change subscription inheritance to override parent methods and add a new field for property_partner

---
+++ luxer_copy_subscription/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Copy Subscription Property',
+    'summary': 'A module to copy the subscription property to the invoice',
+    'description': 'A module to copy the subscription property to the invoice - yall',
+    'author': 'Odoo PS',
+    'category': 'sales subscription',
+    'version': '15.0.0.1',
+    'depends': [
+        'sale_subscription',
+    ],
+    'data': [
+        'views/sale_subscription_inherit.xml',
+    ],
+    'license': 'OPL-1',
+}
