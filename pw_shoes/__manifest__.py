31341216696b86470ffecaae6ea9d0a099713619
sast-odoo <sast@odoo.com>
2022-06-22 15:06:55 -0400

    [MOV] moved to correct repo

---
+++ pw_shoes/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    "name" : "P&W Shoes",
+
+    "depends" : ["product","account"],
+
+    "data" : [
+        "views/product_views_inherit.xml"
+    ],
+
+    "demo" : []
+
+
+}
