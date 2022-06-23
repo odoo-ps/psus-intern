71076a382994bdb8d43f6e7df689d7db0a626b71
nimi-odoo <nimi@odoo.com>
2022-06-23 09:53:42 -0400

    [REF] Record states are now updated functionally

--- tvu_networks/models/sale_order.py
+++ tvu_networks/models/sale_order.py
@@ -3,13 +3,16 @@
 from odoo import models, fields, api
 from datetime import datetime
 
+
 class SaleOrder(models.Model):
     _inherit = "sale.order"
+
+    def set_state(self, state):
+        self.state = state
 
     def cancel_old_quotations(self):
         today = fields.Date.today()
         records = self.env["sale.order"].search([])
         old_quotations = records.filtered(lambda r: r.validity_date).filtered(lambda r: r.validity_date < today) #
-        return old_quotations.write({
-            'state': 'cancel',
-        })
+        for record in old_quotations:
+            record.set_state("cancel")
