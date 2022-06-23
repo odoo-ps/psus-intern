78dd47bc63cda268d7439f86ddb8f489d1ecbbad
damu-odoo <damu@odoo.com>
2022-06-23 15:53:48 -0500

    Fixed search user and partner id, used self.env.ref to locate the xml reference of the user and partner

--- confirm_rfqs/__manifest__.py
+++ confirm_rfqs/__manifest__.py
@@ -3,15 +3,13 @@
     'name': 'Confirm RFQs ',
     'sumary': 'Confirm',
     'description': """
-        Confirm RFQs
+        Confirm RFQs.
     """,
     'author': 'Odoo PS',
     'category': 'Sales',
     'version': '14.0.1.0.0',
     'depends': [
-        'sale',
         'sale_subscription',
-        'product',
         'purchase',
     ],
     'data': [
--- confirm_rfqs/models/purchase_order.py
+++ confirm_rfqs/models/purchase_order.py
@@ -6,5 +6,5 @@ class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
     def confirm_rfqs(self):
-        for order in self.env['purchase.order'].search([('state', '=', 'draft'), ('user_id.id', '=', 1), ('partner_id.id', '=', 1)]):
+        for order in self.env['purchase.order'].search([('state', '=', 'draft'), ('partner_id.id', '=',self.env.ref('base.main_partner').id ), ('user_id.id', '=', self.env.ref('base.user_admin').id)]):
             order.button_confirm()
