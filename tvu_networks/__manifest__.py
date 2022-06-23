15b3e9409ca3f8d7dc075b5e1393ff985f31bbb9
nimi-odoo <nimi@odoo.com>
2022-06-23 09:54:24 -0400

    [FIX] Manifest formatting

--- tvu_networks/__manifest__.py
+++ tvu_networks/__manifest__.py
@@ -1,30 +1,21 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': "TVU Networks",
-
+    'name': "TVU Networks - Autocancellation",
     'summary': """
         Automatically cancels quotations after their expiration date has passed. 
     """,
-
     'description': """
-        Cancels quotations in the Sales app every night at midnight after their validity date has passed
+        Ticket 2874049: Cancels quotations in the Sales app every night at midnight after their validity date has passed
     """,
-
     'author': "Odoo",
     'website': "https://www.odoo.com",
-
+    'license': 'OPL-1',
     'category': 'Sales',
     'version': '0.1',
-
-    'depends': ['base', 'sale'],
-
+    'depends': ['sale'],
     'data': [
-        # 'security/ir.model.access.csv',
         'data/quotation_auto_cancel.xml', 
     ],
-
-    'demo': [
-
-    ],
+    'demo': [],
 }
