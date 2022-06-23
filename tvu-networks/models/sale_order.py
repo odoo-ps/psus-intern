4c494dd0cee31c7b6efb305f1951f57754fb2571
teth-odoo <teth@odoo.com>
2022-06-23 15:27:56 -0400

    added @api.model

--- tvu-networks/models/sale_order.py
+++ tvu-networks/models/sale_order.py
@@ -8,6 +8,6 @@ from odoo import models,fields,api
 >>>>>>> parent of cf12d15 (Auto Cancel The Expired Quotations)
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
-
+    @api.model
     def _auto_cancel_expired_record(self):
         self.search([('state', 'in', ['draft','sent']),('validity_date', '<', fields.Date.today())]).action_cancel()
