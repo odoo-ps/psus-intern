b68ab647f9be9342a7c9d61bd91165f2012d5586
ayhp-odoo <ayhp@odoo.com>
2022-06-23 14:17:49 -0500

    fixed default value on selection on pos view

--- owl-framework/static/src/js/costumer_type.js
+++ owl-framework/static/src/js/costumer_type.js
@@ -1,4 +1,5 @@
-odoo.define("pos_costumer_type.pos_costumer_type", require => {
+alert('hereeeeeeeeee!!!!')
+odoo.define("pos_costumer_type.pos_costumer_type", function(require) {
   "use strict";
   var models = require("point_of_sale.models");
   models.load_fields("res.partner","costumer_type");
