f8a91f6314b9edf068192e17a27f412a30d8fc79
nimi-odoo <nimi@odoo.com>
2022-06-23 10:22:35 -0400

    [REF] Using builtin function for cancelling quotes

--- tvu_networks/models/sale_order.py
+++ tvu_networks/models/sale_order.py
@@ -7,12 +7,9 @@ from datetime import datetime
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    def set_state(self, state):
-        self.state = state
-
     def cancel_old_quotations(self):
         today = fields.Date.today()
         records = self.env["sale.order"].search([])
         old_quotations = records.filtered(lambda r: r.validity_date).filtered(lambda r: r.validity_date < today)
         for record in old_quotations:
-            record.set_state("cancel")
+            record.action_cancel()
