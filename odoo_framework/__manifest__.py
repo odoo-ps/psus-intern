1c5216813a63da6551352f62df06a1293c44a02d
EduardoCedillo(eacm) <eacm@odoo.com>
2022-06-23 11:22:03 -0500

    [ADD] 	-inherit from res.partner and add the field customer_type 	-inherit from res_partner form and add the field custumer_type 	-load the custumer_type to the point_of_sale.models to use it in view front 	-inherit from ClientDetails to the and load the custumer_type

---
+++ odoo_framework/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Odoo Framework',
+    'sumary': 'OWL Framework: POS changes',
+    'description': """
+        odoo framework module adds a new field to the POS screen
+    """,
+    'author': 'EduardoCedillo(eacm)',
+    'category': 'Sales',
+    'version': '15.0.1.0.0',
+    'depends': ['sale'],
+    'license': 'OPL-1',
+    'data': [
+        'views/res_partner_inherit.xml',
+    ],
+    'assets': {
+        'point_of_sale.assets': [
+            'odoo_framework/static/src/js/Custumer_type.js',
+        ],
+        'web.assets_qweb': [
+            'odoo_framework/static/src/xml/ResPartner.xml',
+        ],
+    },
+}
---
+++ odoo_framework/models/res_partner.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    #add type selection field
+    custumer_type = fields.Selection(string='Custumer Type', 
+                               selection=[('consumer', 'Consumer'),
+                                          ('patient', 'Patient'),
+                                          ('caregiver', 'Caregiver'),
+                                          ('externalpatient', 'ExternalPatient')])
