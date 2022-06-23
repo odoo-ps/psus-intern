34a7b1c6e66a08d22e9c2647752c914302b44570
Philip Snyder <phsn@odoo.com>
2022-06-23 10:56:31 -0400

    [ADD] Init Commit

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Materiales Castelar - Quantity Check",
+
+    'summary': """""",
+
+    'description': """
+        Prevents users from entering a bigger quantity than the demand in the Inventory App
+        Task Id: 2874088
+        """,
+
+    'author': 'Philip Snyder',
+
+    'category': 'Custom Development',
+    'version': '1.0',
+    'license': 'OPL-1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['stock'],
+
+    # always loaded
+    # 'data': [
+    # ],
+
+    'application': False,
+}
