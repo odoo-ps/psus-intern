f9b29d9f1d5a22bfd074e29bb11eae1c8da8fc1b
hmpb-odoo <hmpb@odoo.com>
2022-06-23 15:29:09 -0500

    [FIX] Fixing the selection of the id.

--- mindesa_schedule/models/purchase_order.py
+++ mindesa_schedule/models/purchase_order.py
@@ -5,6 +5,9 @@ from odoo import api, fields, models, _
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
-    def _confirmRPQ(self):
-        for order in self.env['purchase.order'].search([('state', '=', 'draft'), ('partner_id.id', '=', '1'), ('user_id.id', '=', '1')]):
+    def _confirm_rfq(self):
+        print(self.env.uid)
+        for order in self.env['purchase.order'].search([('state', '=', 'draft'),
+                                                        ('partner_id.id', '=', self.env.ref('base.main_partner').id),
+                                                        ('user_id.id', '=', self.env.ref('base.user_admin').id)]):
             order.button_confirm()
