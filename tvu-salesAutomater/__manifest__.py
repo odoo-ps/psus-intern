2d75d1f79cc68556f299a89cfdd3f99d013ec9dd
rmal-odoo <rmal@odoo.com>
2022-06-23 11:19:43 -0400

    modified changes as requested

--- tvu-salesAutomater/__manifest__.py
+++ tvu-salesAutomater/__manifest__.py
@@ -8,22 +8,30 @@
     
     'description': """
     Once the current date crosses the expiration date of the sale order that quotation is automtically deleted
+
+    TaskID: 2874120
+    Link: https://www.odoo.com/web#id=2874120&cids=3&menu_id=4720&action=4665&active_id=2874112&model=project.task&view_type=form
     """,
     
-    'author': 'Odoo',
+    'author': 'Odoo Inc',
+
+    'license': 'OPL-1',
     
     'website': 'https://www.odoo.com',
     
     'category': 'Training',
     
     'version': '0.1',
+
+    'application': False,
+
+    'auto-install': True,
+
+    'installable': True,
     
     'depends': ['sale'],
     
     'data': [
         'data/tvu_view.xml'
-    ],
-    
-    'demo': [
-    ],
+    ]
 }
--- tvu-salesAutomater/models/sale_order.py
+++ tvu-salesAutomater/models/sale_order.py
@@ -10,11 +10,6 @@ class SalesOrder(models.Model):
 
 
     def delete_invoice_after_exp_date(self):
-        set_records = self.search([('state','=','draft'),('validity_date','<',date.today())])
-        for rec in set_records:
-            rec.update({
-                'state':'cancel'
-            })
-        # self.env['sale.order'].search([('state', '=', 'draft'),('validity_date', '<', fields.Date.today())]).write({'state': 'cancel'})
+        self.env['sale.order'].search([('state', '=', 'draft'),('validity_date', '<', fields.Date.today())]).write({'state': 'cancel'})
 
 
