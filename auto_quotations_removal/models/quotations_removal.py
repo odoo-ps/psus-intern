e99faaa15509bbf4758170c58d48d48021e06d3a
Fanyang Meng <i@mfy.dev>
2022-06-23 08:54:25 -0400

    Initial Commit

--- auto_quotations_removal/models/quotations_removal.py
+++
@@ -1,9 +0,0 @@
-from odoo import models
-
-class QuotationsRemoval(models.Model):
-    _inherit = 'sale.order'
-
-    def _remove_expired_quotations(self):
-        self.search([('is_expired', '=', True),
-                    ('state', '=', 'draft')]).unlink()
-                    
