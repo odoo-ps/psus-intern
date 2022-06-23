95399774fe78e5c01383399f86ba77a891fa152e
sast-odoo <sast@odoo.com>
2022-06-23 14:14:50 -0400

    [IMP] made the sales price field become readonly at the right time and it recalculates correctly.

--- pw_shoes/models/product_template.py
+++ pw_shoes/models/product_template.py
@@ -2,6 +2,9 @@
 
 from odoo import api, fields, models
 from odoo.exceptions import ValidationError
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class ProductTemplate(models.Model):
     _inherit="product.template"
@@ -16,6 +19,8 @@ class ProductTemplate(models.Model):
 
     price_per_pair = fields.Monetary(string="Price per Pair")
 
+    list_price = fields.Float(compute="_compute_sales_price",store=True)
+
     """
     Sales Price - This field uses the pair per case and price per pair to calculate the Sales price (Pair per price X Price per Pair).
     If nothing is entered in the Pair per Case and Price per Pair, the Sales price should be editable.
@@ -24,8 +29,11 @@ class ProductTemplate(models.Model):
 
 
 
-    @api.onchange("pair_per_case","price_per_pair","list_price")
+    @api.depends("pair_per_case","price_per_pair")
     def _compute_sales_price(self):
         for record in self:
+            _logger.info("************************* Executing compute_sales_price")
             if self.pair_per_case or self.price_per_pair:
+                _logger.info("************************* Pair per case: " + str(self.pair_per_case) + " * Price per pair: " + str(self.price_per_pair))
                 self.list_price = self.pair_per_case * self.price_per_pair
+                _logger.info("************************* = " + str(self.list_price))
