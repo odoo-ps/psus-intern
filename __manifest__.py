cfdca7082ea3794c4efe38fbbcfff8982543e9bc
Philip Snyder <phsn@odoo.com>
2022-06-23 13:25:17 -0400

    [ADD] Init Commit

---
+++ __manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "OWL Framework - POS Changes",
+
+    'summary': """""",
+
+    'description': """
+        Add customer type selection field to backend and pos frontend.
+        """,
+
+    'author': 'Philip Snyder',
+
+    'category': 'Custom Development',
+    'version': '1.0',
+    'license': 'OPL-1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # only loaded in demonstration mode
+    'demo': [],
+    'application': False,
+}
