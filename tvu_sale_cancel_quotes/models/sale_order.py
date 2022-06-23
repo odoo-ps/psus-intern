cfb07ce469813da295641c867f81a36c7bd6a8b1
trwe-odoo <trwe@odoo.com>
2022-06-23 10:31:21 -0400

    fixed bug and cleaned up code

--- tvu_sale_cancel_quotes/models/sale_order.py
+++ tvu_sale_cancel_quotes/models/sale_order.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models
+from datetime import date
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
     def cancel_expired_quotations(self):
-        quotes = self.env['sale.order'].search([('state', '=', 'draft'),('validity_date', '!=', False),('is_expired', '=', True)]).write({'state': 'cancel'})
+        self.env['sale.order'].search([('state', '=', 'draft'),('validity_date', '!=', False),('validity_date','<',date.today())]).write({'state': 'cancel'})
