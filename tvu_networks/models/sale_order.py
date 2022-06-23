a3cd7f66e8466590fbb0927aa68da78cb5b8601e
nimi-odoo <nimi@odoo.com>
2022-06-23 10:09:41 -0400

    [IMP] Removed unneeded comment

--- tvu_networks/models/sale_order.py
+++ tvu_networks/models/sale_order.py
@@ -13,6 +13,6 @@ class SaleOrder(models.Model):
     def cancel_old_quotations(self):
         today = fields.Date.today()
         records = self.env["sale.order"].search([])
-        old_quotations = records.filtered(lambda r: r.validity_date).filtered(lambda r: r.validity_date < today) #
+        old_quotations = records.filtered(lambda r: r.validity_date).filtered(lambda r: r.validity_date < today)
         for record in old_quotations:
             record.set_state("cancel")
