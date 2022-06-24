5345a7868d0043430445eafa67699499be46e6b6
yali-odoo <107009428+yali-odoo@users.noreply.github.com>
2022-06-24 11:04:30 -0400

    [FIX] add newline to the end, and update code
    add exception to capture zero exception and change the file name to product_template.py

--- nyshoes/models/product_template.py
+++ nyshoes/models/product_template.py
@@ -1,7 +1,7 @@
-#-*- encoding: uft-8 -*-
-
-
+#-*- coding: uft-8 -*-
 from odoo import models,fields,api
+from odoo.exceptions import UserError,ValidationError
+
 
 class ProductTemplate(models.Model):
     _inherit='product.template'
@@ -17,10 +17,16 @@ class ProductTemplate(models.Model):
     @api.depends("pair_per_case","price_per_pair")
     def _compute_sales_price(self):
         for rec in self:
+            if rec.pair_per_case<0 or rec.price_per_pair<0:
+               raise UserError("The number can not be negtive!") 
             if rec.pair_per_case and rec.price_per_pair:
                rec.sales_price=rec.pair_per_case*rec.price_per_pair
                
     @api.depends("sales_price")
     def _inverse_compute_sales_price(self):
         for rec in self:
-            rec.pair_per_case=rec.sales_price/rec.price_per_pair    
+            if rec.pair_per_case==0:
+              raise ValidationError('The Pair Per Case can not be zero')
+            else:
+              rec.price_per_pair=rec.sales_price/rec.pair_per_case 
+          
