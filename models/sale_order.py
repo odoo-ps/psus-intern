faf7abebf379f46d006eac0594c66e8c48cf78ab
yeas-odoo <yeas@odoo.com>
2022-06-23 21:07:45 -0400

    [UPDATE] Fixed review comments

--- models/sale_order.py
+++ models/sale_order.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from datetime import date
 
 class SaleOrder(models.Model):
 
@@ -8,7 +9,11 @@ class SaleOrder(models.Model):
     
     def cancel_expired_quotations(self):
 
-        records = self.search([('state','=','draft')])
+        records = self.search([
+            ('state','=','draft'),
+            ('validity_date', '!=', 'False'),
+            ('validity_date', '<=', date.today())
+            ])
 
         for record in records:
             record.action_cancel()
