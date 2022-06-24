7c041cf58239052d2bbccce534eb99285bc06c96
sast-odoo <sast@odoo.com>
2022-06-24 12:56:38 -0400

    [ADD] base module luxer

---
+++ luxer/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name":"Luxer",
+
+    "summary":"Module to add functionality for copying property from subscription to invoice delivery address",
+
+    "description":"Creates a new many2one field on subscription and copies the value to the delivery address if it is set.",
+
+    "author":"Sam Struble",
+
+    "category":"Luxer",
+
+    "version":"1.0",
+
+    "license":"LGPL-3",
+
+    "depends":["invoice","contact","subscription"],
+
+    "auto-install":False,
+
+    "installable":True,
+
+    "application":False,
+
+    "data":[],
+
+    "demo":[]
+}
