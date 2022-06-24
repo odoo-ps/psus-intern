0e4e25af19acf126d0f437bddef9a1f78b9d1814
daav-odoo <107005161+daav-odoo@users.noreply.github.com>
2022-06-24 09:47:23 -0500

    Applying Requested Changes

---
+++ quantity_warehouse/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Materiales Castelar',
+
+    'summary': """Warehouse shouldn't receive more than order""",
+
+    'description': """
+       Materiales Castelar wants to control receipts to make sure the different warehouses won't receive more than the ordered quantity.
+    """,
+
+    'author': 'Odoo PS',
+
+    'website': 'https://www.odoo.com',
+
+    'category': 'Training',
+
+    'version': '15.0.1.0.0',
+
+    'depends': ['stock'],
+
+    'data': [],
+
+    'demo': [],
+}
