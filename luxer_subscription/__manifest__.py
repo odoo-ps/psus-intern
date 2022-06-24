8867d238bdd68594a65a718b991f95b1d88fb7e6
daav-odoo <107005161+daav-odoo@users.noreply.github.com>
2022-06-24 09:40:46 -0500

    Applying Requested Changes

---
+++ luxer_subscription/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Luxer Subscription',
+
+    'author': 'Odoo PS',
+
+    'website': 'https://www.odoo.com',
+
+    'category': 'Training',
+
+    'version': '15.0.1.0.0',
+
+    'depends': [
+        "sale_subscription",
+    ],
+
+    'data': [],
+
+    'demo': [],
+}
