60faba3313b136438889284f8af234e642114fcf
sast-odoo <sast@odoo.com>
2022-06-24 09:43:01 -0400

    [IMP] 2 lines before class and removed unused imports

--- pw_shoes/models/product_template.py
+++ pw_shoes/models/product_template.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models
-from odoo.exceptions import ValidationError
-import logging
+
 
 class ProductTemplate(models.Model):
     _inherit="product.template"
