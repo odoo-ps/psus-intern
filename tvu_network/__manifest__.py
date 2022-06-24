390f9264f923957da6eaad473c1b248a7683de01
daav-odoo <107005161+daav-odoo@users.noreply.github.com>
2022-06-24 09:53:32 -0500

    Applying Requested Changes

---
+++ tvu_network/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Auto-remove quotations',
+
+    'summary': """Auto-Cancel Expired Quotations""",
+
+    'description': """
+To allow for more accurate sales reporting without creating extra administrative work, we would like to automatically cancel quotations which are no longer relevant, as defined by the quotation passing its expiration date.       
+    """,
+
+    'author': 'Odoo PS',
+
+    'website': 'https://www.odoo.com',
+
+    'category': 'Training',
+
+    'version': '15.0.1.0.0',
+
+    'depends': ['sale'],
+
+    'data': [],
+
+    'demo': [],
+}
+
