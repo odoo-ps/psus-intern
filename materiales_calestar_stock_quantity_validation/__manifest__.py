a21f628cc43c523e077aa3270905a1b078e8bc61
yeas-odoo <yeas@odoo.com>
2022-06-24 11:30:35 -0400

    [ADD] Materiales Calestar - Reciept Done Quantity validity check

---
+++ materiales_calestar_stock_quantity_validation/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Materiales Calestar - Validate Quantity for Warehouse",
+    "version": "0.1.0",
+    "description": """
+    #2874450 - 
+    Validates Done Quantity to be less than or equal to Demand (in reciepts).
+    """,
+    "summary": "Field Validation - (Done <= Demand)",
+    "author": "Odoo Inc.",
+    "website": "https://www.odoo.com",
+    "license": "OPL-1",
+    "category": "Custom Development",
+    "depends": ["base", "stock"],
+    "data": [],
+    "demo": [],
+    "auto_install": False,
+    "installable": True,
+    "application": False,
+    "assets": {},
+}
