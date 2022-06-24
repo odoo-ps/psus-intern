a4a77d5947c051d11f196e98655407df99ee446e
rmal-odoo <rmal@odoo.com>
2022-06-24 10:37:26 -0400

    removed link from manifest file and adjusted the spaces

--- NY_P&W_Shoes_AutoCalculated_Price/__manifest__.py
+++ NY_P&W_Shoes_AutoCalculated_Price/__manifest__.py
@@ -7,8 +7,8 @@
     'summary': """ AutoCalculating the sales price for the client """,
     
     'description': """
+    
     ID: 2874139
-    link: https://www.odoo.com/web#id=2874139&cids=3&menu_id=4720&action=4665&active_id=2874112&model=project.task&view_type=form
 
     The client can enter pair per case and price per pair fields in the product.template model if there is any value entered here then the sales price field will be automatically calculated or else it will be editable
     """,
@@ -30,11 +30,13 @@
 
     'installable': True,
     
-    'depends': [
+    'depends': 
+    [
         'product',
         ],
     
-    'data': [
+    'data': 
+    [
         'views/product_template_inherit_views.xml'
-    ],
+        ],
 }
