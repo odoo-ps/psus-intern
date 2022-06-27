27631188fabbd8cd03ac0fe2e82db458a41d8222
hmpb-odoo <hmpb@odoo.com>
2022-06-27 15:08:13 -0500

    [FIX] Fixing a bug where not set a compte field

--- fgd_glass/models/res_partner.py
+++ fgd_glass/models/res_partner.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api, _
+from odoo import models, fields
 
 #class that inherits from the res_partner model
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    is_read_only = fields.Boolean(string="Is read only", compute='_is_read_only', store=False)
+    is_read_only = fields.Boolean(string="Is read only", compute='_compute_is_read_only', store=False)
 
 
-    def _is_read_only(self):
-        for user in self:
-            if user.env.user.has_group ('sales_team.group_sale_salesman_all_leads') or user.env.user.has_group ('sales_team.group_sale_salesman'):
-                user.is_read_only = True 
-            if user.env.user.has_group ('sales_team.group_sale_manager'):
-                user.is_read_only = False
+    def _compute_is_read_only(self):
+        for partner in self:
+            partner.is_read_only = True
+            if partner.env.user.has_group ('sales_team.group_sale_salesman_all_leads') or partner.env.user.has_group ('sales_team.group_sale_salesman'):
+                partner.is_read_only = True 
+            if partner.env.user.has_group ('sales_team.group_sale_manager'):
+                partner.is_read_only = False
