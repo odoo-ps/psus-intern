3ad4331854627c9074db175a9f37fa017c1f988d
sast-odoo <sast@odoo.com>
2022-06-27 09:19:56 -0400

    [IMP] newlines, manifest improvements

--- tvu/__manifest__.py
+++ tvu/__manifest__.py
@@ -1,9 +1,27 @@
 # -*- coding: utf-8 -*-
 
 {
-    "name" : "TVU",
+    "name" : "TVU - Auto-cancel expired orders",
+
+    "summary" : "TVU module to auto-cancel expired orders",
+
+    "description" : "Adds a scheduled function to run every night at midnight (EST) to find orders whose expiration date has passed and changes their state to cancelled",
     
+    "author": "Sam Struble",
+
+    "category":"TVU",
+
+    "version":"1.0",
+
+    "license":"LGPL-3",
+
     "depends" : ["sale"],
+
+    "auto-install":False,
+
+    "installable":True,
+
+    "application":False,
 
     "data" : [
         "views/sale_views_inherit.xml",
--- tvu/models/sale_order.py
+++ tvu/models/sale_order.py
@@ -3,9 +3,7 @@
 from odoo import api, fields, models
 from datetime import datetime
 from datetime import timedelta
-import logging
 
-_logger = logging.getLogger(__name__)
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
@@ -14,13 +12,11 @@ class SaleOrder(models.Model):
 
 # the auto-update cron job to run at midnight every day
     def _cancel_expired_orders(self):
-        _logger.error("Calling cancel expired orders")
-        _logger.error(type(self))
 
-        recordset = self.search([("expiration","!=",False)]) #only return records with set expirations
-        
+        #recordset = self.search([("expiration","!=",False)]) #only return records with set expirations
+
+        recordset = self.search([("expiration","!=",False)])        
 
         for record in recordset:
             if datetime.now().day > record.expiration.day: #if this day is past the expiration day
                     record.state = "cancel"
-
