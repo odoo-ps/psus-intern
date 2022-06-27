494c1b46b99cae0cc79087dabbaddadaabce0a2b
Justin Deng <jden@odoo.com>
2022-06-27 10:26:10 -0400

    [IMP] minor fixes as per feedback

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-# odoo-training
-=======
 # psus-intern
 Internship Project Works Repository 
->>>>>>> e90ff40593ed7e0e42fa87bf6d548367ad7855f2
--- fgd_glass/__manifest__.py
+++ fgd_glass/__manifest__.py
@@ -1,10 +1,13 @@
 # -*- encoding: utf-8 -*-
 
 {
-    'name': 'FGD Glass',
+    'name': 'FGD Glass Field Access Rights',
     'version': '1.0',
-    'author': 'Justin Deng (jden)',
-    'descripton': 'Module to modify behavior of res.partner model to fit FGD_Glass requirements',
+    'author': 'Odoo Inc',
+    'summary': 'Module to modify behavior of res.partner model to fit FGD_Glass requirements',
+    'description': """This module affects the salesperson and pricelist fields in the res.partner model.  It prevents 
+    FGD salespeople from editing the saleslist after a contact has been created. It also forces the salesperson to be the person 
+    who created the contact. FGD admins can edit both of these fields at will.""",
     'category': 'Training',
     'license': 'OPL-1',
     'depends':['base','sale'],
@@ -13,5 +16,4 @@
         'security/ir.model.access.csv',
         'views/res_partner_views.xml'
     ],
-    'demo':[],
 }
