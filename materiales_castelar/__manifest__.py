a74c6ec01d4169080a85f08ecd62431a8ff755aa
mivb-odoo <mivb@odoo.com>
2022-06-27 10:01:38 -0400

    added a validation check to ensure no extra products are supplied

---
+++ materiales_castelar/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Materiales Castelar: Control Receipts",
+
+    'summary': """
+    Materiales Castelar wants to control receipts to make sure the different warehouses won't receive more than the ordered quantity.
+    """,
+
+    'description': """
+        
+    """,
+
+    'author': "Odoo Inc.",
+    'website': "http://www.odoo.com",
+    
+    'category': 'Stock',
+    'version': '1.0',
+
+    'depends': ['stock'],
+
+    'data': [
+        
+    ],
+
+    'license': 'OPL-1'
+}
