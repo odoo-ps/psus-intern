2ec5e569c7599b5f5f3917926474c9ece19e0385
yazh-odoo <yazh@odoo.com>
2022-06-27 14:48:13 -0400

    [ADD] Basic Module

---
+++ greeneville_product_list/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Unique Product List - Greenville Produce',
+
+    'summary': """Make unique product lists for each customer""",
+
+    'description': """
+        Task ID: 2874406
+        ====== TBF
+    """,
+
+    'author': 'Odoo Inc',
+
+    'website': 'https://www.odoo.com',
+
+    'category': 'Custom Development',
+
+    'version': '14.0.1.0',
+
+    'depends': ['sale'],
+
+    'data': [
+
+    ],
+
+    'demo': [
+
+    ],
+
+    'auto_install': False,
+
+    'installable': True,
+
+    'application': False,
+
+    'license': 'OPL-1',
+}
