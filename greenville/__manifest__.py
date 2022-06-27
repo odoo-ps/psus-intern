33d1efaf247ec37ca2288febaf6c725c57415d4b
sast-odoo <sast@odoo.com>
2022-06-27 10:38:02 -0400

    [ADD] basic module

---
+++ greenville/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+
+    "name" : "Greenville Produce - Product Lists",
+
+    "summary" : "",
+
+    "description" : "",
+
+    "author" : "Sam Struble",
+
+    "category" : "Greenville",
+
+    "version" : "0.1",
+
+    "depends" : ["sale"],
+
+    "data" : [],
+
+    "demo" : []
+
+}
---
+++ greenville/models/product_list.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class ProductList(models.Model):
+    _name = "product.list"
+    _description = "Product List"
