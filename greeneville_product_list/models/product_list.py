f693eceea70970c5b15507e644341e3aff90dc1c
yazh-odoo <yazh@odoo.com>
2022-06-28 10:00:46 -0400

    [ADD] ProductList model

---
+++ greeneville_product_list/models/product_list.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+
+class ProductList(models.Model):
+    _name = 'product.list'
+    _description = 'Product List'
+
+    name = fields.Char(string='Name', required=True)
+    # to be finished: Tags
+
+    product_ids = fields.Many2many(comodel_name='product.template',
+                                   string='Product List')
+
+    customer_ids = fields.one2many(comodel_name='res.partner',
+                                   inverse_name='product_list_id',
+                                   string='Customers That Use This List')
