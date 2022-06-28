c1b3187c820de87a00e0ae43fc1200f4e0153ff1
miql-odoo <miql@odoo.com>
2022-06-28 14:52:05 -0400

    [FIX] sequence fields

--- automated_product_name/models/ir_sequence.py
+++ automated_product_name/models/ir_sequence.py
@@ -6,4 +6,5 @@ from odoo import fields, models
 class IrSequence(models.Model):
     _inherit = 'ir.sequence'
 
-    category_id = fields.Integer(string='category id', required=True)
+    category_id = fields.Many2one("product.template", string='category id', required=True)
+    gender = fields.Char("product.template", string='product gender', required=True)
