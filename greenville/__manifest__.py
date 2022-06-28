c3e4da585990ae11d0f71398e884724e6b59bdf3
sast-odoo <sast@odoo.com>
2022-06-28 09:53:17 -0400

    [IMP] added the many2one and one2one fields to the view, improved the product list view, added to the res.partner view to be able to view and select a product list to apply to that customer

--- greenville/__manifest__.py
+++ greenville/__manifest__.py
@@ -14,13 +14,14 @@
 
     "version" : "0.1",
 
-    "depends" : ["sale","base","sale_management"],
+    "depends" : ["sale","base","sale_management","contacts"],
 
     "data" : [
         "security/ir.model.access.csv",
 
         "views/sale_menuitems_inherit.xml",
-        "views/product_list_view.xml"
+        "views/product_list_view.xml",
+        "views/base_view_partner_form_inherit.xml"
 
     ],
 
