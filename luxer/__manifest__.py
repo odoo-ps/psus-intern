d3c0cf65975589ad79c5aed5389ebaed5cd231d8
teth-odoo <teth@odoo.com>
2022-06-28 16:50:01 -0400

    Made few of the necessary changes
    Signed-off-by: teth-odoo <teth@odoo.com>

--- luxer/__manifest__.py
+++ luxer/__manifest__.py
@@ -5,7 +5,7 @@
     'description': """Luxer keeps track of a Contact record, Property Partner they want on each subscription. They want this field copied to the delivery address on the invoice when it is created from subscription.If it is not set, then just keep the same delivery address as normal. Task ID: 2874205""",
     'author': 'Odoo Inc',
     'category': 'Sales',
-    'version': '0.1',
+    'version': '1.0',
     'depends': [
         'sale',
         'sale_subscription'
