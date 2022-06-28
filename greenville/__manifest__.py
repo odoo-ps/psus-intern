1b2441b93d1b9160b16ae1adfb30d07e833d0ac4
hmpb-odoo <hmpb@odoo.com>
2022-06-28 11:43:42 -0500

    [Fix] Minnor problems

--- greenville/__manifest__.py
+++ greenville/__manifest__.py
@@ -10,6 +10,7 @@
     'version': '14.0.1.0.0',
     'depends': [
         'sale',
+        'website',
     ],
     'license': 'OPL-1',
     'data': [
--- greenville/controllers/website_sale_inherit.py
+++ greenville/controllers/website_sale_inherit.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import http
 from odoo.http import request
-# Import the class
 from odoo.addons.website_sale.controllers.main import WebsiteSale, TableCompute
 from odoo.addons.http_routing.models.ir_http import slug
 
