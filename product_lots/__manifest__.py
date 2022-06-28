cfa4bf057199e97bbd9e53b314a1fa8f7e8a6ae1
hmpb-odoo <hmpb@odoo.com>
2022-06-28 11:12:31 -0500

    [ADD] Add secuence yo product template

---
+++ product_lots/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Product Specific Lots',
+    'sumary': 'Add a specific lot for each product created',
+    'description': """
+        App a solt for each product created, with a list of abreviations for each product.
+    """,
+    'author': 'Odoo PS',
+    'category': 'Sales',
+    'version': '14.0.1.0.0',
+    'depends': [
+        'sale',
+    ],
+    'license': 'OPL-1',
+    'data': [
+        "views/product_view_inherit.xml",
+    ],
+}
