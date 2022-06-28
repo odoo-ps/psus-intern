66b46d5ac9fbc35a10d7e2884519ad2845eceb97
Viet Dinh <vidi@odoo.com>
2022-06-28 09:13:24 -0400

    TODO: user_id field doesn't change yet

--- fgd_glass_solutions/models/res_partner.py
+++ fgd_glass_solutions/models/res_partner.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class Partner(models.Model):
@@ -9,10 +9,12 @@ class Partner(models.Model):
     partner_create_only = fields.Boolean(
         compute='_compute_partner_create_only')
 
+    @api.depends()
     def _compute_partner_create_only(self):
+        user = self.env.user
+        writable = user.has_group('sales_team.group_sale_manager') or not (
+            user.has_group('sales_team.group_sale_salesman') or
+            user.has_group('sales_team.group_sale_salesman_all_leads')
+        )
         for record in self:
-            record.partner_create_only = (
-                not record.has_group('sales_team.group_sale_manager') and
-                (record.has_group('sales_team.group_sale_salesman') or
-                 record.has_group('sales_team.group_sale_salesman_all_leads'))
-            )
+            record.partner_create_only = record.id and not writable
