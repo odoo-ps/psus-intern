1faa528dadf75a30de9a745828c9ad8599fc12dc
EduardoCedillo(eacm) <eacm@odoo.com>
2022-06-28 14:20:29 -0500

    [ADD] add prefix to lot number of manufacture products once they are done 	-inherit product.template and add lot_prefix field 	-inhertir mrp.production and add lot_number field 	-inherit mrp.production view and add lot_number to the view 	-inherit product_template view form and add lot_prefix 	-override button_mark_done and add the creacion of the sequence

---
+++ odoo_lot/models/mrp_production.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+
+
+class MRPProduction(models.Model):
+    _inherit = 'mrp.production'
+
+    lot_number = fields.Char(string='Lot Number')
+
+    # when press button mark as done in mrp.production calculate lot number
+    def button_mark_done(self):
+        # execute super function
+        res = super(MRPProduction, self).button_mark_done()
+        # check if lot number is empty
+        if not self.lot_number:
+            # get lot number from product
+            self.lot_number = self.env['ir.sequence'].next_by_code(
+                self.product_id.lot_prefix)
+            if not self.lot_number:
+                self.lot_number = self.env['ir.sequence'].create({
+                    'name': 'Lot Number',
+                    'code': self.product_id.lot_prefix,
+                    'prefix': self.product_id.lot_prefix,
+                    'padding': 5,
+                    'number_increment': 1,
+                    'company_id': self.company_id.id,
+                    'implementation': 'standard',
+                }).next_by_code(self.product_id.lot_prefix)
+        return res
