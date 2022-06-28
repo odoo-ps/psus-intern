f5512ab1e9c887e97145d12f621c1825ad45ba87
daav-odoo <107005161+daav-odoo@users.noreply.github.com>
2022-06-28 17:04:53 -0500

    [IMP] Greenville Produce

---
+++ green/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'green',
+    'sumary': 'Greenville Produce: Unique product lists per customer on website',
+    'description': """
+        odoo product helps you to create a unique product list per customer on website
+    """,
+    'author': 'Odoo PS',
+    'category': 'Sales',
+    'version': '14.0.1.0.0',
+    'depends': ['sale'],
+    'license': 'OPL-1',
+    'data': [
+        'views/greenville_menuitems.xml',
+        'views/product_list_views.xml',
+        'views/res_partner_views_inherit.xml',
+        'security/ir.model.access.csv',
+    ],
+}
