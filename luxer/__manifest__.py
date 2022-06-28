f506b8e12f572772962dbf5b7808ce706ef7cd62
teth-odoo <teth@odoo.com>
2022-06-28 16:29:52 -0400

    Made few of the necessary changes
    Signed-off-by: teth-odoo <teth@odoo.com>

--- luxer/__manifest__.py
+++ luxer/__manifest__.py
@@ -2,10 +2,8 @@
 {
     'name': 'Luxer',
     'sumary': 'Copy properties from res.partner to subscription',
-    'description': """
-        Copy properties from res.partner to subscription
-    """,
-    'author': 'teth',
+    'description': """Luxer keeps track of a Contact record, Property Partner they want on each subscription. They want this field copied to the delivery address on the invoice when it is created from subscription.If it is not set, then just keep the same delivery address as normal. Task ID: 2874205""",
+    'author': 'Odoo Inc',
     'category': 'Sales',
     'version': '0.1',
     'depends': [
@@ -15,8 +13,6 @@
     'license': 'OPL-1',
     'data': [
         'views/sale_subscription_views_inherit.xml',
-    ],
-    'demo': [
     ],
 
 }
--- luxer/models/sale_subscription.py
+++ luxer/models/sale_subscription.py
@@ -1,5 +1,7 @@
 # *-* coding: utf-8 *-*
 from odoo import models, fields, api
+
+
 class SaleSubscription(models.Model):
     _inherit = 'sale.subscription'
     property_partner_id = fields.Many2one(comodel_name='res.partner',string='Property Partner')
@@ -8,5 +10,4 @@ class SaleSubscription(models.Model):
         self.ensure_one()
         vals = super()._prepare_invoice_data()
         vals['partner_id'] = self.property_partner_id.id or vals.get('partner_id')
-        print('successs................................')
         return vals
