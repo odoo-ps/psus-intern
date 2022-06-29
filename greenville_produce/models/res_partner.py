e41677fab3ea8a4e0d60072bb09bfd027d618223
wiwu-odoo <wiwu@odoo.com>
2022-06-29 10:44:46 -0400

    Feature 1

--- greenville_produce/models/res_partner.py
+++ greenville_produce/models/res_partner.py
@@ -6,4 +6,4 @@ from odoo import models, fields, api
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    product_list_id=fields.Many2one(comodel_name="greenville_products.id", string="Product List")
+    product_list_id=fields.Many2one(comodel_name="greenville.products.list", string="Product List")
