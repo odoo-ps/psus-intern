37368f1982bac45900400ded6ccceed4aadc20c9
“Sunhyeok <suju@odoo.com>
2022-06-29 09:45:48 -0400

    [FIX] Modified code files based on mentor's feedback

--- tvu/__manifest__.py
+++ tvu/__manifest__.py
@@ -6,29 +6,23 @@
         For Client TVU""",
     'description': """
         This module provides Auto cancel expired quotations for TVU.
+        Task ID : 2874159
     """,
 
-    'author': "Sunhyeok Jung",
+    'author': "Odoo Inc",
     'website': "http://www.odoo.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.2',
+    'category': 'Custom Development',
+    'version': '1.0',
 
-    # any module necessary for this one to work correctly
     'depends': ['base','sale'],
 
-    # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
         'views/expired_cancel.xml'
     ],
-    # only loaded in demonstration mode
     'demo': [
-        # 'demo/demo.xml',
     ],
 }
--- tvu/models/__init__.py
+++ tvu/models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import tvu
+from . import sale_order
