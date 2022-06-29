ad99e9a9048fabb31935517e4c938ba74a060c87
sast-odoo <sast@odoo.com>
2022-06-29 14:25:20 -0400

    [IMP] restored paging functionality to the shop page

--- greenville/controllers/greenville_controllers.py
+++ greenville/controllers/greenville_controllers.py
@@ -10,29 +10,42 @@ _logger = logging.getLogger(__name__)
 class WebsiteSaleInherit(WebsiteSale):
     #@http.route("/shop/", auth="public", website=True)
     @http.route()
-    def shop(self, **kw):
-        products = http.request.env["product.list"].search([])
-        res=super().shop()
+    #def shop(self, category=None, search="", **kw):
+    def shop(self, page=0, category=None, search='', min_price=0.0, max_price=0.0, ppg=False, **post): #taken from the inherited func in odoo/addons/website_sale/controllers/main.py
+        #products = http.request.env["product.list"].search([])
+        res=super(WebsiteSaleInherit, self).shop(page=page, category=category, search=search, min_price=min_price, max_price=max_price, ppg=ppg)
+
         user=http.request.env.user.partner_id.id
         _logger.critical("User ID: " + str(user))
         #_logger.critical(res)
 
         #product_lists = http.request.env['product.list'].search([])
 
-        product_list = http.request.env['product.list'].search([('customer_ids','=',user)]) #contains the list ID of the user (If there is one)
-
+        #product_list = http.request.env['product.list'].search([('customer_ids','=',user)]) #contains the list ID of the user (If there is one)
+        product_list = http.request.env['res.partner'].search([("id","=",user)]).product_list_id
         
+        context = dict(http.request.context)
+
+        _logger.critical(str(http.request.context))
 
         if (product_list):
             _logger.critical("TRUE")
-            _logger.critical("List ID: " + product_list.name)
+            _logger.critical("List name: " + product_list.name)
             #res.qcontext.update({"products:", product_list})
-            context = dict(http.request.context)
-            context.update({"products" : product_list})
+            #context = dict(http.request.context)
+            
+            _logger.critical("IDs: " + str(product_list.product_ids))
+
+            for product in product_list.product_ids:
+                _logger.critical("Product name: " + product.name)
+            #res.qcontext.update({"products" : product_list.product_ids})
+            context["products"] = product_list.product_ids
             http.request.context=context
+
         else:
             _logger.critical("false")
 
+        #Product_list now contains the products in the product list that this person is using.
 
         
 
@@ -55,4 +68,6 @@ class WebsiteSaleInherit(WebsiteSale):
         
         
         return res
+        #return http.request.render("website_sale.products",{"products":product_list.product_ids, "category":category})
+        #return http.request.render("website_sale.products",context)
         #return "hi there :)"
