cb1fd0236f8637f3dd925493ad00919dc4ac9744
yixi-odoo <yixi@odoo.com>
2022-06-29 14:44:37 -0400

    [ADD] web controller

---
+++ Greenville_product_uniquelist/models/product_list_controller.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# -*- coding: utf-8 -*-
+from odoo import http
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+
+
+class ProductList(WebsiteSale):
+
+    
+    def _get_search_domain(self, search, category, attrib_values, search_in_description=True):
+        domain = super(ProductList, self)._get_search_domain(search, category, attrib_values, search_in_description)
+        product_list_id = http.request.env.user.partner_id.product_list_id
+        if product_list_id:
+            domain.append(('id', 'in', product_list_id.product_list.ids))
+        return domain
+        
