40b8e9fd44d8c42c2114d1558383b4dc2557a9f2
daav-odoo <107005161+daav-odoo@users.noreply.github.com>
2022-06-29 16:58:22 -0500

    Delete psl directory

--- psl/models/product_template.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8 
-
-from odoo import fields,models
-
-class ProductTemplate(models.Model):
-  _inherit = 'product.template'
-
-  lot_number_prefix = fields.Char(string="Lot NUmber Prefix")
