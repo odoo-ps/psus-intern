58ea2637cad1731d53a03cb103eb6ff1c165a78b
wiwu-odoo <wiwu@odoo.com>
2022-06-29 10:26:57 -0400

    Initial commit

---
+++ greenville_produce/__manifest__.py
@@ -0,0 +1,26 @@
+#-*- coding: utf-8 -*-
+
+{
+    'name': 'Greenville Produce',
+
+    'version': '1.0',
+
+    'depends': ['sale'],
+
+    'category': 'Inventory',
+
+    'author': 'Odoo Inc',
+
+    'description': """TASK ID: 2874296
+
+    """,
+
+    'data': [
+        'views/action_menuitem.xml',
+        'views/product_product_list.xml',
+        'views/product_list_inherit.xml',
+    ],
+    
+    'license': 'OPL-1',
+
+}
---
+++ greenville_produce/models/res_partner.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    product_list_id=fields.Many2one(comodel_name="greenville_products.id", string="Product List")
