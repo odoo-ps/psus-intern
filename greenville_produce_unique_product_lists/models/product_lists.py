c9b4be939870759631cb931a6df78ddcbc348e1a
yawa <yawa@odoo.com>
2022-06-29 09:19:53 -0400

    [IMP] description to new model product_lists

--- greenville_produce_unique_product_lists/models/product_lists.py
+++ greenville_produce_unique_product_lists/models/product_lists.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-from odoo.exceptions import UserError, ValidationError
 
 
 class ProductLists(models.Model):
     _name = 'greenville.products_list'
-    _description = 'some description'
+    _description = 'Greenville Products List'
 
     name = fields.Char(string='Title', required=True)
     product_template_ids = fields.Many2many(
-        'product.product', string='Product Template')
+        'product.template', string='Product Template')
     customers_using_list = fields.One2many(
-        'res.partner', 'products_list_id', string='Customers Using List', auto_join=True)
+        'res.partner', 'products_list_id', string='Customers Using List')
