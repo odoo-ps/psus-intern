14a9c947f1dd4fb798366af5e618ebea2a663e41
yawa <yawa@odoo.com>
2022-06-29 09:17:44 -0400

    [IMP] manifest description and security

--- greenville_produce_unique_product_lists/__manifest__.py
+++ greenville_produce_unique_product_lists/__manifest__.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': "Greenville Produce: Unique product lists per customer",
+    'name': "Greenville Produce - Unique product lists per customer",
     'summary': """
     """,
     'description': """
         task_id: 2874333
+        
+        Added a new tab with a new field 'Product List' to 'res.partner'
+        Created a new model for products list
+        Added a new menuitem for 'Product List' under Sales -> Products
+        Inherited '_get_search_domain' method of WebsiteSale controller to filter according to Product List assigned to the user
     """,
     'author': 'Odoo Inc',
     'website': 'https://www.odoo.com/',
@@ -15,6 +20,8 @@
     'license': 'OPL-1',
     'depends': ['sale'],
     'data': [
+        'security/product_list_security.xml',
+        'security/ir.model.access.csv',
         'views/product_lists_menuitem.xml',
         'views/product_lists_views.xml',
         'views/contact_views_inherit.xml',
