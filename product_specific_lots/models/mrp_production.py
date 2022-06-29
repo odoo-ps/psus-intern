1f9ce652d19160c6f485e0da92a3f9ce9a6d772c
daav-odoo <107005161+daav-odoo@users.noreply.github.com>
2022-06-29 17:00:48 -0500

    [IMP] product_specific_lots: An auto-product specific lot setter
    When manufacturing products, the finished goods need to have lot numbers that correlate with the product for record keeping purposes. Additionally due to the volume and daily operations, it needs to be automatically assignable without knowing what the previous lot number for that item was. They would then count up independent of each other.

---
+++ product_specific_lots/models/mrp_production.py
@@ -0,0 +1,23 @@
+from odoo import fields, models
+
+
+class MrpProduction(models.Model):
+  _inherit = 'mrp.production'
+
+  lot_number = fields.Char(string="Lot Number")
+
+  def button_mark_done(self):
+    res = super(MrpProduction, self).button_mark_done()
+    if self.product_id.lot_number_prefix:
+      self.lot_number = self.env['ir.sequence'].next_by_code(self.product_id.lot_number_prefix)
+      if not self.lot_number:
+        self.lot_number = self.env['ir.sequence'].create({
+          'name': 'Lot Counter',
+          'code': self.product_id.lot_number_prefix,
+          'prefix': self.product_id.lot_number_prefix,
+          'padding': 5,
+          'number_increment': 1,
+          'implementation': 'standard'
+        }).next_by_code(self.product_id.lot_number_prefix)
+
+    return res
