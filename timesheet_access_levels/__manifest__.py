10e83c016904b571886507e6239f74bcedb0ddd4
Justin Deng <jden@odoo.com>
2022-06-29 14:54:01 -0400

    [IMP] implmeneted user notifcation when timesheet is invalidated

---
+++ timesheet_access_levels/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+
+{
+    'name': 'Timesheet Validation Pipeline',
+    'version': '1.0',
+    'license': 'OPL-1',
+    'author': 'Odoo Inc',
+    'summary': 'Implmented timesheet pipeline based on permissions',
+    'description': """
+        There are several levels of approval for timesheets. A project manager must approve it, and then a supervisor, and then finally 
+        and administrator. If the timesheet is invalidated at any point, it will be reset to a draft and the creator of the timesheet
+        will be notified.
+    """,
+    'depends': ['account', 'hr_timesheet', 'mail'],
+    'data': [
+        'security/access-levels-security.xml',
+        'security/ir.model.access.csv',
+        'views/account_analytic_line_views.xml'
+    ],
+}
+
+
+
--- timesheet_access_levels/models/account_analytic_line.py
+++ timesheet_access_levels/models/account_analytic_line.py
@@ -4,17 +4,13 @@ from odoo import tools, models, fields, api, _
 from odoo.exceptions import AccessError
 class ModifiedAnalyticLine(models.Model):
     _inherit='account.analytic.line'
-    _description='modifed anayltic acocunt line'
-
-    #use built in methods to valid and invalidate timesheets
-    #ping specific user when time is right
 
 
     current_level = fields.Selection(selection = [
-                                      ('1', 'access-levels.group_access_levels_pm'),
-                                      ('2', 'access-levels.group_access_levels_supervisor'),
-                                      ('3', 'access-levels.group_access_levels_admin'),
-                                      ], default='1')
+                                      ('1', 'timesheet_access_levels.group_access_levels_pm'),
+                                      ('2', 'timesheet_access_levels.group_access_levels_supervisor'),
+                                      ('3', 'timesheet_access_levels.group_access_levels_admin'),
+                                      ], default='1',string="Needs approval from")
 
     user_can_validate = fields.Boolean(compute='_compute_can_validate',
         help="Whether or not the current user can validate/reset to draft the record.")
@@ -22,6 +18,10 @@ class ModifiedAnalyticLine(models.Model):
     invoice_ready = fields.Boolean(required=True, default=False)
 
     levels_of_approval = fields.Integer(default=3)
+
+    messenger = fields.Many2one(comodel_name ='mail.thread',ondelete='cascade')
+
+    
 
     def _compute_can_validate(self):
         print('validate function overwritten')
@@ -33,6 +33,14 @@ class ModifiedAnalyticLine(models.Model):
                     line.user_can_validate = True
             else:
                 line.user_can_validate = False
+    
+    @api.depends('user_can_validate')
+    def write(self, vals):    
+        if not self.user_can_validate:
+            raise AccessError('You are attempting to edit a timesheet you do not have access to')
+        else:
+            res = super(ModifiedAnalyticLine, self).write(vals)
+            return res
 
 
     @api.depends('user_can_validate')
@@ -51,10 +59,31 @@ class ModifiedAnalyticLine(models.Model):
     def action_invalidate_timesheet(self):
         currLevelIntValue = int(self.current_level)
         if self.user_can_validate and currLevelIntValue >= 1: 
-            self.current_level = str(currLevelIntValue - 1)
+            if currLevelIntValue > 1:
+                self.current_level = str(currLevelIntValue - 1)
             people_to_notify = [self.create_uid]
-            if self.current_level == '2':
-                people_to_notify.append(self.employee_id.timesheet_manager_id.id)
-            self.message_post(body="A timesheet that you approved was invalidated. Please review it.", partner_ids=[(6,self.create_uid)])
+            invalidator_id = self.env.user.partner_id.id
+            message_text='Your timesheet was invalidated. Please review and resubmit it.'
+
+            channel = self.env['mail.channel'].sudo().search([
+                (self.create_uid.partner_id.id, 'in', 'channel_partner_ids')
+            ],
+                limit=1,
+            )
+            if not channel:
+                channel = self.env['mail.channel'].with_context(mail_create_nosubscribe=True).sudo().create({
+                    'channel_partner_ids': [(4, self.create_uid.partner_id.id)],
+                    'public': 'private',
+                    'channel_type': 'chat',
+                    'name': f'Timesheet Invalidated',
+                    'display_name': f'Timesheet Invalidated',
+                })
+
+            channel.sudo().message_post(
+                body=message_text,
+                author_id=invalidator_id,
+                message_type="comment",
+            )
+
         else:
             raise AccessError(_("You are trying to invalidate a timesheet that you do not have permission to"))
