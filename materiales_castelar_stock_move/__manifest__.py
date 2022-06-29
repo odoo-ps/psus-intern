505f99ebd58a517987e850d4c1fd85cd65687506
“Sunhyeok <suju@odoo.com>
2022-06-29 10:10:32 -0400

    [EDIT] materiales_castelar_stock_move : edit code files based on mentor's feedback

---
+++ materiales_castelar_stock_move/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "materiales_castelar_stock_move",
+    'summary': """
+        """,
+    'description': """
+        This prevents users from entering the quantity more than demand
+        task id : 2874162
+    """,
+    'author': "Odoo Inc",
+    'website': "http://www.odoo.com",
+    'category': 'Custom Development',
+    'version': '1.0',
+    'depends': ['base','stock'],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
