a28923db0e5fafdf3c21f05ff97f2cd26144ad05
jden-odoo <107053023+jden-odoo@users.noreply.github.com>
2022-06-30 10:26:36 -0400

    [REM] removed extra file/folder

--- fgd_glass/models/sale_order.py
+++
@@ -1,8 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-from odoo import api,fields,models
-
-class linkedSale(models.Model):
-    _inherit = 'sale.order'
-
-    customer_orders = fields.Many2one(comodel_name='res.partner', ondelete='cascade')
