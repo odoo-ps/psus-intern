e96aeb9ed4266e215fc01910cd5ea2cb6ec0c4bf
yawa <yawa@odoo.com>
2022-06-30 14:40:03 -0400

    [ADD] Custom qWeb template module

---
+++ bottling_experts_sale_custom_invoice_pdf/__manifest__.py
@@ -0,0 +1,19 @@
+ {
+    'name': "Bottling Experts - Custom Quotation and Invoice Qweb PDF Reports",
+    'summary': """ 
+        Custom Quotation and Invoice Qweb PDF Reports
+    """,
+    'description': """
+        task_id: 2874345
+    """,
+    'author': 'Odoo Inc',
+    'website': 'https://www.odoo.com/',
+    'category': 'Training',
+    'application': False,
+    'version': '0.1.0',
+    'license': 'OPL-1',
+    'depends': ['sale'],
+    'data': [
+        'views/report_invoice.xml',
+    ],
+}
