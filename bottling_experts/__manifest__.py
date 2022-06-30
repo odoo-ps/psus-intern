c93b56bbd861c6958ef93ea9675453c6430d4c2d
daav-odoo <107005161+daav-odoo@users.noreply.github.com>
2022-06-30 13:24:08 -0500

    [IMP] Bottling Experts: Custom Quotation and Invoice Qweb PDF Reports
    Client uses specialized quotation and invoice templates that they are unable to replicated to their requirements in the Studio report editor.
    This is essential to their operations in managing complex B2B service requests.

---
+++ bottling_experts/__manifest__.py
@@ -0,0 +1,30 @@
+{
+  'name':'Bottling Experts',
+
+  'summary':'''An specialized quotation and invoice template''',
+
+  'description':'''
+     Client uses specialized quotation and invoice templates that they are unable 
+     to replicated to their requirements in the Studio report editor.
+     This is essential to their operations in managing complex B2B service requests.
+  ''',
+
+  'author':'Odoo PS',
+  
+  'category':'Sales',
+
+  'website':'https://www.odoo.com',
+  
+  'version':'14.0.1.0.0',
+
+  'depends':[
+  	'sale',
+  ],
+
+  "data":[
+    'views/sale_order_views.xml',
+    'report/sale_order_report_templates.xml',
+  ],
+  
+   'license':'OPL-1',
+}
---
+++ bottling_experts/models/sale_order.py
@@ -0,0 +1,9 @@
+from odoo import fields, models
+
+
+class SaleOrder(models.Model):
+  _inherit = 'sale.order'
+
+  travel_in = fields.Date(string="Travel In")
+  travel_out = fields.Date(string="Travel Out")
+  opportunity_name = fields.Char(string="Opportunity name")
