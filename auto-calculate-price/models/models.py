ef7aeda3b9229fcb4217f8c77c3516fcad614df7
Hazique Quazi <mohq@odoo.com>
2022-07-01 11:57:23 -0400

    [ADD] Completed logic for conditionally enabled list_price field

--- auto-calculate-price/models/models.py
+++ auto-calculate-price/models/models.py
@@ -9,12 +9,12 @@ class ProductTemplate(models.Model):
     pair_per_case = fields.Integer(string="Pairs of shoes in a case")
     price_per_pair = fields.Float(string="Price of a pair of shoes")
 
-    # list_price = fields.Float(
-    #         'Sales Price', default=1.0,
-    #         digits='Product Price',
-    #         help="Price at which the product is sold to customers.",
-    #         compute="_compute_sales_price"
-    #     )
+    list_price = fields.Float(
+            'Sales Price', default=1.0,
+            digits='Product Price',
+            help="Price at which the product is sold to customers.",
+            compute="_compute_sales_price"
+        )
 
     @api.depends('pair_per_case', 'price_per_pair')
     def _compute_sales_price(self):
