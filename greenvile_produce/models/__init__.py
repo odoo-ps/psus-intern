89e228a45af3673cc23cf5fdba2935abcb9f1d72
Justin Deng <jden@odoo.com>
2022-07-01 15:13:17 -0400

    [IMP] bug fixes and removed files

--- greenvile_produce/models/__init__.py
+++ greenvile_produce/models/__init__.py
@@ -1,4 +1,3 @@
 from . import product_list
 from . import res_partner
-#from . import controller
 from . import product_template
--- greenvile_produce/models/product_template.py
+++ greenvile_produce/models/product_template.py
@@ -1,4 +1,6 @@
-from odoo import models
+from odoo import models,fields
 
 class ProductTemplate(models.Model):
     _inherit='product.template'
+
+    lists = fields.Many2many(comodel_name='greenvile.produce.productlist',relation='product_template_product_list_rel',column1='product_template_id',column2='product_list_id')
