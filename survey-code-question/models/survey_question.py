d1e220da12c9a189c415baa941df6fed60412588
yall-odoo <yall@odoo.com>
2022-07-06 14:26:17 -0500

    [CHG] survey-code-question: correcting inheritance
    Correcting inheritance with SurveyUserInput and SurveyUserInputLine. Also changing question type to code_box to be in pair with text_box.

--- survey-code-question/models/survey_question.py
+++ survey-code-question/models/survey_question.py
@@ -1,22 +1,38 @@
 # -*- coding: utf-8 -*-
 
+import logging
 from odoo import api, fields, models, _
 
 class SurveyQuestion(models.Model):
     _inherit = 'survey.question'
 
-    question_type = fields.Selection(selection_add=[('code', 'Coding question')])
+    question_type = fields.Selection(selection_add=[('code_box', 'Coding question')])
+
+    # TODO possible test suite
+    answer_code_box = fields.Char('Correct coding answer', help='Correct output for this coding question.')
+
+    @api.depends('question_type', 'scoring_type', 'answer_date', 'answer_datetime', 'answer_numerical_box', 'answer_code_box')
+    def _compute_is_scored_question(self):
+        super()._compute_is_scored_question()
+        for question in self:
+            if question.question_type == 'code_box' and question.answer_code_box:
+                question.is_scored_question = True
+            else:
+                question.is_scored_question = False
 
     def validate_question(self, answer, comment=None):
         res = super().validate_question(answer, comment)
         if not res:
-            if self.question_type == 'code':
+            if self.question_type == 'code_box':
                 return self._validate_code_box(answer)
             else:
                 return {}
+        else:
+            return res
 
     def _validate_code_box(self, answer):
-        #FIXME answer is an empty list -> it may be due to lack of save_lines handling of 'code' case OR _get_line_values.
+        #FIXME answer is an empty list
+        logging.info(msg=f'Answer received for validation: {answer}')
         try:
             code_output = eval(answer)
         except:
--- survey-code-question/models/survey_user_input.py
+++ survey-code-question/models/survey_user_input.py
@@ -1,12 +1,34 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models, _
+import logging
+
+
+class SurveyUserInput(models.Model):
+    _inherit = 'survey.user_input'
+
+
+    def save_lines(self, question, answer, comment=None):
+        super().save_lines(question, answer, comment)
+
+        old_answers = self.env['survey.user_input.line'].search([
+            ('user_input_id', '=', self.id),
+            ('question_id', '=', question.id)
+        ])
+
+        if question.question_type in ['code_box']:
+            logging.info(msg='Is code box yessir')
+            self._save_line_simple_answer(question, old_answers, answer)
+        
+        super().save_lines(question, answer, comment)
+
+    def _get_line_answer_values(self, question, answer, answer_type):
+        vals = super()._get_line_answer_values(question, answer, answer_type)
+        logging.info(msg=f'Vals: {vals}')
+        return vals
 
 class SurveyUserInputLine(models.Model):
     _inherit = 'survey.user_input.line'
 
     answer_type = fields.Selection(selection_add=[('code_box', _('Code Text box'))])
-
     value_code_box = fields.Char('Code answer')
-
-    #TODO (i think) inherit save_lines and _get_line_answer_values
