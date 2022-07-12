8364899d52eddb15398c8329c412b52676274c85
ayhp-odoo <ayhp@odoo.com>
2022-07-12 08:50:16 -0500

    Added functionality for managing responses on test button

--- api_connector/api-module/models/base_automation.py
+++ api_connector/api-module/models/base_automation.py
@@ -20,11 +20,39 @@ class BaseAutomation(models.Model):
         headers = self.env['ir.actions.server'].translate_o2m(
             raw_headers) if raw_headers else None
 
+        errors = {
+            400: "Bad Request",
+            401: "Unauthorized",
+            403: "Forbidden",
+            404: "Not Found",
+            405: "Method Not Allowed",
+            408: "Request Timeout",
+            500: "Internal Server Error",
+            502: "Bad Gateway",
+            503: "Service Unavailable",
+            511: "Network Authentication Required",
+        }
+
         if not url or not method:
             raise UserError('API must have a url and method')
+
         try:
             if method == 'get':
-                response = requests.get(url, params=params)
+                response = requests.get(url, headers=headers, params=params)
+            else:
+                response = requests.post(url, headers=headers, data=payload)
+            
+            if response.status_code >= 200 and response.status_code < 400:
                 action.response = response.text
+            elif response.status_code in errors:
+                action.response = response.text
+                raise UserError(
+                    errors[response.status_code] + " -> " + response.text)
+            else:
+                action.response = response.text
+                raise Exception("Unknow Error ->" + response.text)
+                
         except Exception as e:
-            raise UserError(e)
+            action.response = response.text
+            raise UserError(
+                "Something went wrong please retry or check the logs")
