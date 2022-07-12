aabf6b42ce0b7c3e8787302799e9acfbb384826e
sast-odoo <sast@odoo.com>
2022-07-12 10:26:04 -0400

    [ADD] 'Add this field' button logs the current field hierarchy so we can locate subfields-of-subfields (doesnt yet add it to the many2many)

--- custom_edi/models/fields_inherit.py
+++ custom_edi/models/fields_inherit.py
@@ -24,6 +24,26 @@ class ModelField(models.Model):
 
     # is called to open the "view fields" form on sub-fields
     def open_form(self):
+        
+        #if "field_tree" in self.env.context:
+        #    self.env.context["field_tree"] += "/" + str(self.id)
+        #else:
+        #    self.env.context["field_tree"] = str(self.id)
+        
+        #if "field_tree" in self.env.context:
+        #    self = self.with_context({"field_tree" : self.env.context["field_tree"]+"/"+str(self.id)})
+        #else:
+        #    self = self.with_context({"field_tree": str(self.id)})
+
+
+        #Adds a new variable to the context to keep track of the hierarchy of subfields so we can add it
+        # is like this: [ top_model_id, second_model_id, third_model_id, etc ]
+        ctx = self.env.context.copy()
+        if "field_tree" in ctx:
+            ctx["field_tree"].append(self.id)
+        else:
+            ctx["field_tree"] = [self.id]
+            
         return {
             #'name': 'Fields',
             'name': self.field_description,
@@ -33,5 +53,20 @@ class ModelField(models.Model):
             'domain': [('model_id', '=', self.relation)],
             'view_mode': 'tree',
             'target': 'current',
-            'res_id' : self.id
+            'res_id' : self.id,
+            'context' : ctx
         }
+    def add(self):
+        _logger.error("Add button pressed on " + self.field_description)
+        #_logger.error(self.env.context)
+        #_logger.error("Field Tree: " + str(self.env.context["field_tree"]))
+
+        if "field_tree" in self.env.context:
+            m="FIELD TREE: "
+            for model_id in self.env.context["field_tree"]:
+                m = m + self.env["ir.model.fields"].browse(model_id)[0].field_description + "/"
+            _logger.error(m)
+
+            self.field_tree = "/".join(str(self.env.context["field_tree"]))
+
+        #return {'type': 'ir.actions.act_window_close'}
