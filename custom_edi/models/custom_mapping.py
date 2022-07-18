9cfdf5939dd53c5af1226f18a8daccc612d6b300
sast-odoo <sast@odoo.com>
2022-07-18 13:45:55 -0400

    [IMP] refactored code a bit to make add_field act more of an add_tag function to make it more flexible which allowed me to fix the bug of subtags-of-subtags-of-x2m not being added

--- custom_edi/models/custom_mapping.py
+++ custom_edi/models/custom_mapping.py
@@ -101,15 +101,17 @@ class CustomMapping(models.Model):
 
                 tag_element = ET.SubElement(record_element, tag.xml_tag)
                 
-                if tag.is_static:
-                    tag_element.text = tag.static_content
+                #if tag.is_static:
+                #    tag_element.text = tag.static_content
 
-                else: #only non-static fields get their set fields evaluated
-                    if tag.field_id or tag.field_tree: #this tag contains a field
-                        self.add_field(tag_element, tag, record)
+                #else: #only non-static fields get their set fields evaluated
+
+                #if tag.field_id or tag.field_tree: #this tag contains a field
+                self.add_field(tag_element, tag, record)
                 
-                if len(tag.child_tag_ids) > 0: #this tag contains children tags that we must process
-                    self.add_children_tags(tag_element, tag, record)
+                #this is now done from add_field
+                #if len(tag.child_tag_ids) > 0: #this tag contains children tags that we must process
+                #    self.add_children_tags(tag_element, tag, record)
 
                 #end `for tag`
 
@@ -142,16 +144,18 @@ class CustomMapping(models.Model):
         # record - current record whose data we're exporting (this method is meant to be called from the main loop as it only processes 1 record)
 
         #print("Record:",record.name,record)
+
+        this_field = False
+        this_value = False
+        name_path = ""
+
         if tag.field_tree: #if a field tree is set
-
-
             path = tag.field_tree.split("/") # path is now a list of field names in order, ie ['field1','field2']
             name_path = tag.field_tree #for printing
 
             this_field = self.env["ir.model.fields"].search([("model_id.model","=",self.model.model),("name","=",path[0])])
 
             this_value = record[path[0]]
-            
 
             for path_index in range(1,len(path)): # go 1 subfield deeper each iteration
                 this_value = this_value[path[path_index]]
@@ -166,19 +170,27 @@ class CustomMapping(models.Model):
             this_field = tag.field_id #the actual 'field' obj of the tag model set by the user (only gets used if there is no tree, so this should be non relational)
             this_value = record[this_field.name]
             name_path = this_field.name
-        
-        
-        print("~~~~~ Field: " + name_path + " ~~~~~")
-        print("Value: " + str(this_value))
-        print("Tag: " + str(tag.xml_tag))
 
-        print("Ttype:", this_field.ttype)
-        print("Py type:", type(this_value))
-        print("Path:",name_path)
+        elif tag.is_static:
+            this_value = tag.static_content
+        
+        print("~~~~~ Tag:", tag.xml_tag,"~~~~~")
+        if this_value: print("Value:", this_value)
+        else: print("Value: None")
 
+        print("Path:", name_path)
+
+        if this_field:
+            print("Ttype:", this_field.ttype)
+            print("Py type:", type(this_value))
+        if tag.is_static:
+            print("This field is static!")
+
+        print("Children:", len(tag.child_tag_ids))
+        print("XML subtags:", len(tag.x2many_subtag_ids))
 
         ########### x2m handling
-        if str(this_field.ttype) in ("one2many","many2many"):
+        if this_field and str(this_field.ttype) in ("one2many","many2many"):
         # When a user sets a tag to export a x2many field (i.e. either one2many or many2many), we now have to iterate through
         #   every record in the x2m, INSIDE the main iteration through every record of the main model.
         # If a edi_tag is a x2many, the x2many_subtag_ids of the edi_tag will hold the list of sub-edi_tags that describe what pieces
@@ -198,28 +210,43 @@ class CustomMapping(models.Model):
                 #This is iterating through each edi_tag that the user wants exported for every record in the x2m.
 
                     tag_element = ET.SubElement(record_element, subtag.xml_tag)
-                    print("****** Subtag:",subtag)
-                    print("****** Subtag.xml_tag:",subtag.xml_tag)
+                    #print("****** Subtag:",subtag)
+                    #print("****** Subtag.xml_tag:",subtag.xml_tag)
                     
+                    print("Is static: ", subtag.is_static)
                     if subtag.is_static:
                         tag_element.text = subtag.static_content
+                        print("static tag text set to", subtag.static_content)
 
                     else: #only non-static fields get their set fields evaluated.
-                        if subtag.field_id or subtag.field_tree: #this tag contains a field
-                            self.add_field(tag_element, subtag, record)
-                    
-                    if len(subtag.child_tag_ids) > 0: #this tag contains children tags that we must process
-                        self.add_children_tags(tag_element, subtag, record)
+                        #if subtag.field_id or subtag.field_tree: #this tag contains a field
+                        self.add_field(tag_element, subtag, record)
         ###########
         
+        if len(tag.child_tag_ids) > 0:
+            self.add_children_tags(root, tag, record)
 
         #date formatting to YYYY-MM-DD
+
+        if tag.is_static:
+            root.text=str(this_value)
+            return
+
         if this_value and this_field.ttype in ("date", "datetime"): #field has a value, and is a date type
             this_value = this_value.strftime('%Y-%m-%d')
 
+
         #if this field is set to "False" but it's NOT supposed to be a Boolean field, it's probably just not set, so don't export it
-        if (not (this_value==False and this_field.ttype!="boolean")) and not (str(this_field.ttype) in ("one2many","many2many")):
-            root.text = str(this_value)
+        if (this_value==False and not this_field) or\
+            (this_value==False and this_field and this_field.ttype!="boolean"):
+            return
+
+        #If this field is a x2m field, this_value is a odoo recordset which we don't want exported into the xml.
+        if (this_field and this_field.ttype in ("one2many","many2many")):
+            return
+
+        
+        root.text=str(this_value)
 
         return
 
@@ -237,9 +264,20 @@ class CustomMapping(models.Model):
         for child_tag_id in parent_tag.child_tag_ids:
             new_child_tag = ET.SubElement(root, child_tag_id.xml_tag)
 
-            if child_tag_id.field_id or child_tag_id.field_tree: #if this child has a field OR a field tree set
-                self.add_field(new_child_tag, child_tag_id, record)
+            #if child_tag_id.field_id or child_tag_id.field_tree: #if this child has a field OR a field tree set
+            self.add_field(new_child_tag, child_tag_id, record)
 
-            if len(child_tag_id.child_tag_ids) > 0: #if this child tag has children of its own, add all those children, and their children, and their children etc etc
-                self.add_children_tags(new_child_tag, child_tag_id, record)
+            #if len(child_tag_id.child_tag_ids) > 0: #if this child tag has children of its own, add all those children, and their children, and their children etc etc
+            #    self.add_children_tags(new_child_tag, child_tag_id, record)
+
+        """
+        for subtag in parent_tag.x2many_subtag_ids:
+            tag_element = ET.SubElement(root, subtag.xml_tag)
+
+            if subtag.field_id or subtag.field_tree:
+                self.add_field(tag_element, subtag, record)
+            
+            if len(subtag.child_tag_ids) > 0:
+                self.add_children_tags(tag_element, subtag, record)
+        """
         return
