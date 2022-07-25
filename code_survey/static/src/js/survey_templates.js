dd3325b22f0d4fb0230c3bb7d770f3aa25032c87
daav-odoo <daav@odoo.com>
2022-07-25 16:35:01 -0500

    [FIX] code_survey: Fixing the bug in the print template. Moreover, refactoring the code in order to make it easier to read and adding the possibility to have multiple lines of code in the code questions.

--- code_survey/static/src/js/survey_templates.js
+++
@@ -1,7 +0,0 @@
-(async ({}) => {
-                
-    const {default: HighlightedCode} =
-        await import('https://unpkg.com/highlighted-code');
-    
-    HighlightedCode.useTheme('monokai-sublime'); // Themes --> https://github.com/highlightjs/highlight.js/tree/main/src/styles
-    })(self);
